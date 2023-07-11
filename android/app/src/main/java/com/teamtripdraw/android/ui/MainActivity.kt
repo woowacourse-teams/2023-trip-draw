078ce2ca27265d09191e4440cb74ca36344a00de
changhwan <chang5hustle@gmail.com>
2023-07-11 22:40:05 +0900

    [chore] Http client 기초세팅 / 패키징 정리 (#3)

--- android/app/src/main/java/com/teamtripdraw/android/ui/MainActivity.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.teamtripdraw.android
+package com.teamtripdraw.android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.teamtripdraw.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
