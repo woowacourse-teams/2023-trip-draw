b399a99186f483306e6578dcf83cd067fb15fca8
changhwan <chang5hustle@gmail.com>
2023-07-11 13:31:15 +0900

    [feat] 안드로이드 관련 기초세팅 / app 모듈 version catalog  적용 (#1)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 
 android {
     namespace = "com.teamtripdraw.android"
-    compileSdk = 33
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.teamtripdraw.android"
-        minSdk = 26
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.appVersion.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -41,6 +41,18 @@ android {
     viewBinding {
         enable = true
     }
+    // mockk 충돌문제
+    packagingOptions {
+        resources.excludes.add("META-INF/LICENSE.md")
+        resources.excludes.add("META-INF/LICENSE-notice.md")
+    }
+    testOptions {
+        packagingOptions {
+            jniLibs {
+                useLegacyPackaging = true
+            }
+        }
+    }
 }
 
 dependencies {
@@ -49,12 +61,17 @@ dependencies {
     implementation(project(":support"))
 
     // 안드로이드 기본 의존성
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    testImplementation("org.assertj:assertj-core:3.22.0")
+    implementation(libs.bundles.androidDefault)
+
+    // 테스트 의존성
+    implementation(libs.bundles.basicTest)
+
+    // 안드로이드 테스트 의존성
+    implementation(libs.bundles.androidTest)
+
+    // HttpClient 관련 의존성
+    implementation(libs.bundles.httpClient)
+
+    // Glide
+    implementation(libs.glide)
 }
