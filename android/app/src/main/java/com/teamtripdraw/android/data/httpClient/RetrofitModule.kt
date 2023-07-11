d2cdfe00256a3f453d39f39df68505063edd162f
changhwan <chang5hustle@gmail.com>
2023-07-11 22:38:51 +0900

    [feat] Http client 기초세팅 / 레트로핏 모듈 기초세팅(callAdapter 제외) (#3)

---
+++ android/app/src/main/java/com/teamtripdraw/android/data/httpClient/RetrofitModule.kt
@@ -0,0 +1,50 @@
+package com.teamtripdraw.android.data.httpClient
+
+import com.squareup.moshi.Moshi
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+object RetrofitModule {
+//    로그인 작업시 유저 토큰 붙여주는 역할
+//    private val authorizationInterceptor: Interceptor =
+//        Interceptor { chain ->
+//            with(chain) {
+//                proceed(
+//                    request()
+//                        .newBuilder()
+//                        .addHeader("Authorization","유저 토큰 삽입")
+//                        .build()
+//                )
+//            }
+//        }
+
+    private val httpLoggingInterceptor: HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.HEADERS }
+
+    private val tripDrawDispatcher = Dispatcher().apply {
+        maxRequestsPerHost = 10
+    }
+
+    private val tripDrawOkHttpClient: OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .dispatcher(tripDrawDispatcher)
+            .build()
+
+    private val moshi = Moshi.Builder().build()
+
+    private val tripDrawRetrofit: Retrofit =
+        Retrofit.Builder()
+            .baseUrl("")
+            .client(tripDrawOkHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+}
