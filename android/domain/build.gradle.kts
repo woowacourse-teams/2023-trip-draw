799c6b8d58f2aed2dd75611bd4ed2e7199509512
changhwan <chang5hustle@gmail.com>
2023-07-11 16:39:14 +0900

    [feat] 안드로이드 관련 기초세팅 / app 모듈 version catalog  적용 (#1)

--- android/domain/build.gradle.kts
+++ android/domain/build.gradle.kts
@@ -3,9 +3,7 @@ plugins {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
-    testImplementation("org.assertj", "assertj-core", "3.22.0")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+    testImplementation(libs.bundles.domainTest)
 }
 
 tasks {
