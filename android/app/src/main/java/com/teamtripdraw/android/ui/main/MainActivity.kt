1351814bbb213f2b7d6e7b6b010ccd70635662e8
changhwan <chang5hustle@gmail.com>
2023-07-14 22:52:13 +0900

    [feat] 메인 화면_홈 / bottom navigation setOnItemSelectedListener 기본설정  (#7)

--- android/app/src/main/java/com/teamtripdraw/android/ui/main/MainActivity.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/main/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.teamtripdraw.android.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
 import com.teamtripdraw.android.R
+import com.teamtripdraw.android.databinding.ActivityMainBinding
+import com.teamtripdraw.android.ui.main.MainActivity.BottomNavigationMenuType.HISTORY
+import com.teamtripdraw.android.ui.main.MainActivity.BottomNavigationMenuType.HOME
+import com.teamtripdraw.android.ui.main.MainActivity.BottomNavigationMenuType.MY_PAGE
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initBottomNavigation() {
+        binding.bnMain.setOnItemSelectedListener {
+            when (BottomNavigationMenuType.resourceIdToMenuType(it.itemId)) {
+                HOME -> {
+                    return@setOnItemSelectedListener true
+                }
+                HISTORY -> {
+                    return@setOnItemSelectedListener true
+                }
+                MY_PAGE -> {
+                    return@setOnItemSelectedListener true
+                }
+            }
+        }
+    }
+
+    private enum class BottomNavigationMenuType(@IdRes private val resourceId: Int) {
+        HOME(R.id.menu_bn_home),
+        HISTORY(R.id.menu_bn_history),
+        MY_PAGE(R.id.menu_bn_my_page);
+
+        companion object {
+            fun resourceIdToMenuType(@IdRes resourceId: Int): BottomNavigationMenuType =
+                values().find { it.resourceId == resourceId }
+                    ?: throw IllegalStateException(WRONG_BOTTOM_NAVIGATION_RESOURCE_ID_ERROR)
+
+            private const val WRONG_BOTTOM_NAVIGATION_RESOURCE_ID_ERROR =
+                "잘못된 바텀 네비게이션 resourceId 입니다."
+        }
     }
 }
