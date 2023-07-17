83717e82866d4a1fec613906eeca4b8e85a397a3
changhwan <chang5hustle@gmail.com>
2023-07-17 21:38:28 +0900

    [feat] 메인 화면_홈 / controller 버튼 작업 (#7)

--- android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.teamtripdraw.android.R
+import com.teamtripdraw.android.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
