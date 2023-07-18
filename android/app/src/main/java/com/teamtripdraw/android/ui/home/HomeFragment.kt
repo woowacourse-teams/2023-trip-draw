23367a38cc4fc2ead2e01958b2bffbb73653815a
changhwan <chang5hustle@gmail.com>
2023-07-19 01:52:18 +0900

    [feat] 메인 화면_홈 / 위치 권한 받아오는 로직 (#15)

--- android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeFragment.kt
@@ -1,16 +1,38 @@
 package com.teamtripdraw.android.ui.home
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.teamtripdraw.android.databinding.FragmentHomeBinding
+import com.teamtripdraw.android.support.framework.presentation.extemsions.checkForeGroundPermission
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(ACCESS_FINE_LOCATION, false) -> {
+                moveToCurrentPoint()
+            }
+            permissions.getOrDefault(ACCESS_COARSE_LOCATION, false) -> {
+                moveToCurrentPoint()
+                // 세부 항목을 사용 하면 더 정확한 지도 서비스 를 이용할 수 있다는 다이얼로그
+            }
+            else -> {
+                // 권한 받아야 한다는 다이얼로그 띄워주기
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +41,19 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkForeGroundPermission(
+            requireContext(),
+            locationPermissionRequest,
+            this::moveToCurrentPoint
+        )
+    }
+
+    private fun moveToCurrentPoint() {
+        // 네이버 맵 현재 지점 이동 로직 구현
     }
 
     override fun onDestroyView() {
---
+++ android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/extemsions/LocationPermissions.kt
@@ -0,0 +1,62 @@
+package com.teamtripdraw.android.support.framework.presentation.extemsions
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
+import com.teamtripdraw.android.support.framework.presentation.extemsions.LocationPermissionState.ALL_PERMISSION
+import com.teamtripdraw.android.support.framework.presentation.extemsions.LocationPermissionState.HAS_COARSE
+import com.teamtripdraw.android.support.framework.presentation.extemsions.LocationPermissionState.NO_PERMISSION
+
+private const val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+private const val FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
+
+fun checkForeGroundPermission(
+    context: Context,
+    activityResultLauncher: ActivityResultLauncher<Array<String>>,
+    moveToCurrentPoint: () -> Unit
+) {
+    val coarseLocationState =
+        ContextCompat.checkSelfPermission(context, COARSE_LOCATION) == PERMISSION_GRANTED
+    val fineLocationState =
+        ContextCompat.checkSelfPermission(context, FINE_LOCATION) == PERMISSION_GRANTED
+    processLocationPermission(coarseLocationState, fineLocationState, activityResultLauncher, moveToCurrentPoint)
+}
+
+private fun processLocationPermission(
+    coarseLocationState: Boolean,
+    fineLocationState: Boolean,
+    activityResultLauncher: ActivityResultLauncher<Array<String>>,
+    moveToCurrentPoint: () -> Unit
+) {
+    when (LocationPermissionState.get(coarseLocationState, fineLocationState)) {
+        NO_PERMISSION -> {
+            activityResultLauncher.launch(arrayOf(FINE_LOCATION, COARSE_LOCATION))
+        }
+        HAS_COARSE -> {
+            moveToCurrentPoint()
+            activityResultLauncher.launch(arrayOf(FINE_LOCATION))
+        }
+        ALL_PERMISSION -> {
+            moveToCurrentPoint()
+        }
+    }
+}
+
+enum class LocationPermissionState {
+    NO_PERMISSION, HAS_COARSE, ALL_PERMISSION;
+
+    companion object {
+        private const val WRONG_LOCATION_PERMISSION_STATE_ERROR = "위치 정보 권한 상태가 올바르지 않습니다."
+
+        fun get(coarseLocationState: Boolean, fineLocationState: Boolean): LocationPermissionState {
+            return when {
+                !coarseLocationState && !fineLocationState -> NO_PERMISSION
+                coarseLocationState && !fineLocationState -> HAS_COARSE
+                coarseLocationState && fineLocationState -> ALL_PERMISSION
+                else -> throw IllegalStateException(WRONG_LOCATION_PERMISSION_STATE_ERROR)
+            }
+        }
+    }
+}
