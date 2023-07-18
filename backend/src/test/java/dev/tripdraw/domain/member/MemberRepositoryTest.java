a5959757598da497266dd5c8d24586b6eda79199
ReO <yt930310@gmail.com>
2023-07-18 11:29:45 +0900

    [test] 닉네임으로 회원을 조회하는 기능 테스트 작성 (#9)

---
+++ backend/src/test/java/dev/tripdraw/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package dev.tripdraw.domain.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void 닉네임으로_회원을_조회한다() {
+        //given
+        Member 통후추 = new Member("통후추");
+        memberRepository.save(통후추);
+
+        //when
+        Member member = memberRepository.findByNickname("통후추");
+
+        //then
+        assertThat(member).isEqualTo(통후추);
+    }
+}
