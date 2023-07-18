80c4262a3ba84c5b37d2d69d8935ed35a3d290a0
Combi153 <jcm04@naver.com>
2023-07-18 20:34:51 +0900

    [feat] 위치 정보 저장 기능 추가 (#12)

---
+++ backend/src/main/java/dev/tripdraw/application/TripService.java
@@ -0,0 +1,37 @@
+package dev.tripdraw.application;
+
+import static dev.tripdraw.exception.ExceptionCode.NOT_FOUND;
+
+import dev.tripdraw.domain.member.Member;
+import dev.tripdraw.domain.trip.Point;
+import dev.tripdraw.domain.trip.Trip;
+import dev.tripdraw.domain.trip.TripRepository;
+import dev.tripdraw.dto.LoginUser;
+import dev.tripdraw.dto.request.PointCreateRequest;
+import dev.tripdraw.dto.response.PointCreateResponse;
+import dev.tripdraw.exception.BadRequestException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class TripService {
+
+    private final TripRepository tripRepository;
+    private final MemberService memberService;
+
+    public PointCreateResponse addPoint(LoginUser loginUser, PointCreateRequest pointCreateRequest) {
+        Trip trip = tripRepository.findById(pointCreateRequest.tripId())
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND));
+        Member member = memberService.validateLoginUser(loginUser);
+        Point point = pointCreateRequest.toPoint();
+
+        trip.validateAuthorization(member);
+        trip.add(point);
+
+        tripRepository.flush();
+        return PointCreateResponse.from(point);
+    }
+}
---
+++ backend/src/test/java/dev/tripdraw/application/TripServiceTest.java
@@ -0,0 +1,71 @@
+package dev.tripdraw.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import dev.tripdraw.domain.member.Member;
+import dev.tripdraw.domain.member.MemberRepository;
+import dev.tripdraw.domain.trip.Trip;
+import dev.tripdraw.domain.trip.TripRepository;
+import dev.tripdraw.dto.LoginUser;
+import dev.tripdraw.dto.request.PointCreateRequest;
+import dev.tripdraw.dto.response.PointCreateResponse;
+import dev.tripdraw.exception.BadRequestException;
+import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@Transactional
+@SpringBootTest
+class TripServiceTest {
+
+    @Autowired
+    private TripService tripService;
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Trip trip;
+    private LoginUser loginUser;
+
+    @BeforeEach
+    void setUp() {
+        Member member = new Member("통후추");
+        memberRepository.save(member);
+        this.trip = tripRepository.save(new Trip("통후추의 여행", member));
+        loginUser = new LoginUser("통후추");
+    }
+
+    @Test
+    void 여행에_위치_정보를_추가한다() {
+        // given
+        PointCreateRequest pointCreateRequest = new PointCreateRequest(trip.getId(), 1.1, 2.2, LocalDateTime.now());
+
+        // when
+        PointCreateResponse pointCreateResponse = tripService.addPoint(loginUser, pointCreateRequest);
+
+        // then
+        assertThat(pointCreateResponse.id()).isNotNull();
+    }
+
+    @Test
+    void 여행에_위치_정보를_추가할_때_해당_여행이_존재하지_않으면_예외를_발생시킨다() {
+        // given
+        Long nonExistentId = Long.MIN_VALUE;
+        PointCreateRequest pointCreateRequest = new PointCreateRequest(nonExistentId, 1.1, 2.2, LocalDateTime.now());
+
+        // expect
+        assertThatThrownBy(() -> tripService.addPoint(loginUser, pointCreateRequest))
+                .isInstanceOf(BadRequestException.class);
+    }
+}
