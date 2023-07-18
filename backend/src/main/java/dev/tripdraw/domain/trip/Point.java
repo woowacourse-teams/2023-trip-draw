a54ebff3430c48ba5d9f16057eec9126d0e9bb34
Combi153 <jcm04@naver.com>
2023-07-18 20:31:11 +0900

    [feat] 위치 정보 저장 기능 관련 DTO 추가 (#12)

--- backend/src/main/java/dev/tripdraw/domain/trip/Point.java
+++ backend/src/main/java/dev/tripdraw/domain/trip/Point.java
@@ -6,6 +6,7 @@ import dev.tripdraw.domain.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,5 +30,32 @@ public class Point extends BaseEntity {
         this.latitude = latitude;
         this.longitude = longitude;
         this.recordedAt = recordedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Objects.equals(latitude, point.latitude) && Objects.equals(longitude, point.longitude)
+                && Objects.equals(recordedAt, point.recordedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude, recordedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", recordedAt=" + recordedAt +
+                '}';
     }
 }
---
+++ backend/src/main/java/dev/tripdraw/dto/request/PointCreateRequest.java
@@ -0,0 +1,19 @@
+package dev.tripdraw.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import dev.tripdraw.domain.trip.Point;
+import java.time.LocalDateTime;
+
+public record PointCreateRequest(Long tripId, Double latitude, Double longitude,
+                                 @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+                                 LocalDateTime recordedAt) {
+
+    public Point toPoint() {
+        return Point.builder()
+                .latitude(latitude)
+                .longitude(longitude)
+                .recordedAt(recordedAt)
+                .build();
+    }
+}
