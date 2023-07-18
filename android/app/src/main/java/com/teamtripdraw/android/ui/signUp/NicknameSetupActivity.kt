edc2bdd12b20a8795571889d79dad7590be39563
pingu244 <pingu244@naver.com>
2023-07-18 16:06:45 +0900

    [ui] 닉네임 설정_기본 / 초기 닉네임 설정 화면 xml 작업(#22)

---
+++ android/app/src/main/java/com/teamtripdraw/android/ui/signUp/NicknameSetupActivity.kt
@@ -0,0 +1,12 @@
+package com.teamtripdraw.android.ui.signUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamtripdraw.android.R
+
+class NicknameSetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nickname_setup)
+    }
+}
