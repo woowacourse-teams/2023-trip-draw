474486e94e13b5fa31bc5a64846d1668e6842b6a
Woojin Lee <dhkep03@gmail.com>
2023-07-19 17:00:36 +0900

    [docs] 여행 생성 API 설명 추가 (#29)

--- backend/src/main/java/dev/tripdraw/presentation/controller/TripController.java
+++ backend/src/main/java/dev/tripdraw/presentation/controller/TripController.java
@@ -15,20 +15,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Trip", description = "여행 관 API 명세")
+@Tag(name = "Trip", description = "여행 관련 API 명세")
 @RequiredArgsConstructor
 @RestController
 public class TripController {
 
     private final TripService tripService;
 
+    @Operation(summary = "여행 생성 API", description = "현재 로그인한 사용자의 여행을 생성합니다.", tags = {"여행", "로그인"})
     @PostMapping("/trips")
     public ResponseEntity<TripCreateResponse> create(@Auth LoginUser loginUser) {
         TripCreateResponse response = tripService.create(loginUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @Operation(summary = "위치 정보 저장 API", description = "현재 진행 중인 여행의 경로에 위치 정보를 저장합니다..", tags = {"여행", "로그인"})
+    @Operation(summary = "위치 정보 저장 API", description = "현재 진행 중인 여행의 경로에 위치 정보를 저장합니다..", tags = {"여행", "위치", "로그인"})
     @PostMapping("/points")
     public ResponseEntity<PointCreateResponse> addPoint(
             @Auth LoginUser loginUser,
