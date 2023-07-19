91432be5e56d107d7f15a2463a9004f2d01a6faf
ReO <yt930310@gmail.com>
2023-07-19 16:00:40 +0900

    [feat] validation 예외 처리 기능 구현 (#38)
    Co-authored-by: greeng00se <thegreengoose7@gmail.com>

--- backend/src/main/java/dev/tripdraw/exception/common/ExceptionResponse.java
+++ backend/src/main/java/dev/tripdraw/exception/common/ExceptionResponse.java
@@ -1,4 +1,18 @@
 package dev.tripdraw.exception.common;
 
-public record ExceptionResponse(String exceptionCode, String message) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.Collections;
+import java.util.List;
+
+public record ExceptionResponse(
+        String exceptionCode,
+        String message,
+
+        @JsonInclude(Include.NON_EMPTY)
+        List<ValidationException> exceptions
+) {
+    public static ExceptionResponse of(String exceptionCode, String message) {
+        return new ExceptionResponse(exceptionCode, message, Collections.emptyList());
+    }
 }
--- backend/src/main/java/dev/tripdraw/exception/common/GlobalExceptionHandler.java
+++ backend/src/main/java/dev/tripdraw/exception/common/GlobalExceptionHandler.java
@@ -1,16 +1,24 @@
 package dev.tripdraw.exception.common;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final String INTERNAL_SERVER_ERROR_MESSAGE = "서버가 응답할 수 없습니다.";
+    private static final String METHOD_ARGUMENT_NOT_VALID_MESSAGE = "잘못된 요청입니다.";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -19,7 +27,31 @@ public class GlobalExceptionHandler {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         log.error(e.getMessage(), e);
         return ResponseEntity.status(httpStatus)
-                .body(new ExceptionResponse(httpStatus.name(), INTERNAL_SERVER_ERROR_MESSAGE));
+                .body(ExceptionResponse.of(httpStatus.name(), INTERNAL_SERVER_ERROR_MESSAGE));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(
+                        HttpStatus.BAD_REQUEST.name(),
+                        METHOD_ARGUMENT_NOT_VALID_MESSAGE,
+                        generateValidationExceptions(e)
+                ));
+    }
+
+    private List<ValidationException> generateValidationExceptions(BindException e) {
+        return e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(ValidationException::from)
+                .toList();
     }
 
     @ExceptionHandler(BaseException.class)
@@ -27,6 +59,6 @@ public class GlobalExceptionHandler {
         ExceptionType exceptionType = e.getExceptionType();
         log.warn(e.getMessage(), e);
         return ResponseEntity.status(exceptionType.getHttpStatus())
-                .body(new ExceptionResponse(exceptionType.name(), exceptionType.getMessage()));
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.getMessage()));
     }
 }
