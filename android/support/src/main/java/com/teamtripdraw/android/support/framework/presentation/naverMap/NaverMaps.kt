e9d2088fa8166ac67a006a33426f7c95634b1374
changhwan <chang5hustle@gmail.com>
2023-07-19 22:22:29 +0900

    [feat] 메인 화면_홈 / 패키지 오타수정 (#15)

--- android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/naverMap/NaverMaps.kt
+++ android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/naverMap/NaverMaps.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.util.FusedLocationSource
-import com.teamtripdraw.android.support.framework.presentation.extemsions.LocationPermissionState.ALL_PERMISSION
-import com.teamtripdraw.android.support.framework.presentation.extemsions.LocationPermissionState.HAS_COARSE
-import com.teamtripdraw.android.support.framework.presentation.extemsions.LocationPermissionState.NO_PERMISSION
-import com.teamtripdraw.android.support.framework.presentation.extemsions.getCurrentLocationPermissionState
+import com.teamtripdraw.android.support.framework.presentation.extensions.LocationPermissionState.ALL_PERMISSION
+import com.teamtripdraw.android.support.framework.presentation.extensions.LocationPermissionState.HAS_COARSE
+import com.teamtripdraw.android.support.framework.presentation.extensions.LocationPermissionState.NO_PERMISSION
+import com.teamtripdraw.android.support.framework.presentation.extensions.getCurrentLocationPermissionState
 
 const val LOCATION_PERMISSION_REQUEST_CODE = 1000
 
