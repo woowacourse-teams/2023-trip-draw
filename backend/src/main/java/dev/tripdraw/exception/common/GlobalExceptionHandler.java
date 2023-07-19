46a59f64dc92d87679649556d2cb5339c0a085ad
ReO <yt930310@gmail.com>
2023-07-19 15:14:22 +0900

    [feat] 로그 적용 (#38)
    Co-authored-by: greeng00se <thegreengoose7@gmail.com>

--- backend/src/main/java/dev/tripdraw/exception/common/GlobalExceptionHandler.java
+++ backend/src/main/java/dev/tripdraw/exception/common/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package dev.tripdraw.exception.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +12,12 @@ public class GlobalExceptionHandler {
 
     private static final String INTERNAL_SERVER_ERROR_MESSAGE = "서버가 응답할 수 없습니다.";
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception e) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        log.error(e.getMessage(), e);
         return ResponseEntity.status(httpStatus)
                 .body(new ExceptionResponse(httpStatus.name(), INTERNAL_SERVER_ERROR_MESSAGE));
     }
@@ -20,6 +25,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ExceptionResponse> handleBaseException(BaseException e) {
         ExceptionType exceptionType = e.getExceptionType();
+        log.warn(e.getMessage(), e);
         return ResponseEntity.status(exceptionType.getHttpStatus())
                 .body(new ExceptionResponse(exceptionType.name(), exceptionType.getMessage()));
     }
