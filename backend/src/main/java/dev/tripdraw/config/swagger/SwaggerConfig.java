97bf0cd622a2caf953a220e0a6c87fa570e4ce91
Woojin Lee <dhkep03@gmail.com>
2023-07-20 14:39:00 +0900

    [refactor] SwaggerConfig 메서드 분리 (#29)

--- backend/src/main/java/dev/tripdraw/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/dev/tripdraw/config/swagger/SwaggerConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.StringSchema;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,27 +14,28 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        final Info info = new Info()
-                .title("트립드로우 서버 API 명세서")
-                .version("v0.0.1")
-                .description("API Description");
-
-        Header customHeader = new Header();
-        customHeader.setDescription("Basic : Basic BASE64(nickname)");
-        customHeader.schema(new StringSchema());
+        final Info info = createInfo();
 
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
                 .components(
                         new Components()
-                                .addHeaders("Basic", customHeader)
-                )
+                                .addHeaders("Basic", createCustomizedHeader()))
                 .info(info);
     }
 
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                .bearerFormat("BASE64")
-                .scheme("basic");
+    private Info createInfo() {
+        final Info info = new Info()
+                .title("트립드로우 서버 API 명세서")
+                .version("v0.0.1")
+                .description("API Description");
+        return info;
+    }
+
+    private Header createCustomizedHeader() {
+        Header customHeader = new Header();
+        customHeader.setDescription("Basic : Basic BASE64(nickname)");
+        customHeader.schema(new StringSchema());
+        return customHeader;
     }
 }
