9a514c7ca770d2e6c0adc236a98b01fa0a741b06
changhwan <chang5hustle@gmail.com>
2023-07-21 03:11:21 +0900

    [feat] 닉네임 설정 / ResponseState Process 함수 Result 반환 형태로 변경(#43)

--- android/app/src/main/java/com/teamtripdraw/android/data/httpClient/retrofitAdapter/ResponseState.kt
+++ android/app/src/main/java/com/teamtripdraw/android/data/httpClient/retrofitAdapter/ResponseState.kt
@@ -23,14 +23,14 @@ sealed class ResponseState<out T : Any> {
     // 위의 상황에 해당하지 않는 오류 상황
     data class UnknownError(val error: Throwable) : ResponseState<Nothing>()
 
-    fun process(
-        failureListener: (code: Int, errorBody: ResponseBody?) -> Unit = this::defaultFailureListener,
-        networkErrorListener: (error: UnknownHostException) -> Unit = this::defaultNetworkErrorListener,
-        timeOutErrorListener: (error: SocketTimeoutException) -> Unit = this::defaultTimeOutErrorListener,
-        unknownErrorListener: (error: Throwable) -> Unit = this::defaultUnknownErrorListener,
-        successListener: (body: T, headers: Headers) -> Unit = this::defaultSuccessListener
-    ) {
-        when (this) {
+    fun <R> process(
+        failureListener: (code: Int, errorBody: ResponseBody?) -> Result<Nothing> = this::defaultFailureListener,
+        networkErrorListener: (error: UnknownHostException) -> Result<Nothing> = this::defaultNetworkErrorListener,
+        timeOutErrorListener: (error: SocketTimeoutException) -> Result<Nothing> = this::defaultTimeOutErrorListener,
+        unknownErrorListener: (error: Throwable) -> Result<Nothing> = this::defaultUnknownErrorListener,
+        successListener: (body: T, headers: Headers) -> Result<R>
+    ): Result<R> {
+        return when (this) {
             is Success -> {
                 successListener(this.body, this.headers)
             }
@@ -49,22 +49,32 @@ sealed class ResponseState<out T : Any> {
         }
     }
 
-    private fun defaultFailureListener(code: Int, errorBody: ResponseBody?) {
+    private fun defaultFailureListener(code: Int, errorBody: ResponseBody?): Result<Nothing> {
         Log.d("ResponseState.Failure", "code : $code, body : $errorBody")
+        return Result.failure(
+            IllegalAccessException(
+                FAILURE_EXCEPTION_MESSAGE_FORMAT.format(code, errorBody?.string() ?: "")
+            )
+        )
     }
 
-    private fun defaultNetworkErrorListener(error: UnknownHostException) {
+    private fun defaultNetworkErrorListener(error: UnknownHostException): Result<Nothing> {
         Log.e("ResponseState.NetworkError", "error : $error, message : ${error.message}")
         // 네트워크 연결 요청 액티비티 이동로직 혹은 네트워크 끊김 메시지 알림 로직-> 추후작성
+        return Result.failure(error)
     }
 
-    private fun defaultTimeOutErrorListener(error: SocketTimeoutException) {
+    private fun defaultTimeOutErrorListener(error: SocketTimeoutException): Result<Nothing> {
         Log.e("ResponseState.TimeOutError", "error : $error, message : ${error.message}")
+        return Result.failure(error)
     }
 
-    private fun defaultUnknownErrorListener(error: Throwable) {
+    private fun defaultUnknownErrorListener(error: Throwable): Result<Nothing> {
         Log.e("ResponseState.UnknownError", "error : $error, message : ${error.message}")
+        return Result.failure(error)
     }
 
-    private fun defaultSuccessListener(body: T, headers: Headers) {}
+    companion object {
+        private const val FAILURE_EXCEPTION_MESSAGE_FORMAT = "code : %d , errorBody: %s"
+    }
 }
