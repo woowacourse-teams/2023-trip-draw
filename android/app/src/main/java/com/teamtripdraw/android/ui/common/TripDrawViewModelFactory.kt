e0012c28650d7084b7a7f0cb6d6a4e68f08626b2
changhwan <chang5hustle@gmail.com>
2023-07-22 14:35:41 +0900

    [feat] 닉네임 설정 / tripDrawViewModelFactory생성 (#43)

---
+++ android/app/src/main/java/com/teamtripdraw/android/ui/common/TripDrawViewModelFactory.kt
@@ -0,0 +1,24 @@
+package com.teamtripdraw.android.ui.common
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.teamtripdraw.android.TripDrawApplication
+import com.teamtripdraw.android.ui.signUp.NicknameSetupViewModel
+
+private const val UNDEFINED_VIEW_MODEL_ERROR = "ViewModelFactory에 정의되지않은 뷰모델을 생성하였습니다 : %s"
+
+@Suppress("UNCHECKED_CAST")
+val tripDrawViewModelFactory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T =
+        with(modelClass) {
+            val dependencyContainer = TripDrawApplication.DependencyContainer
+            val repositoryContainer = dependencyContainer.repositoryContainer
+            when {
+                isAssignableFrom(NicknameSetupViewModel::class.java) ->
+                    NicknameSetupViewModel(repositoryContainer.setNickNameRepository)
+                else ->
+                    throw IllegalArgumentException(UNDEFINED_VIEW_MODEL_ERROR.format(modelClass.name))
+            }
+        } as T
+}
