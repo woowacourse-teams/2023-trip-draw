5d1fbaf3d9bc6d4c5dca33ef23bbfed3f9061d2e
otter <koreanotter6@gmail.com>
2023-07-26 13:55:34 +0900

    [ui] 닉네임 설정 / 초기 xml 작업 (#71)

---
+++ android/app/src/main/java/com/teamtripdraw/android/ui/post/PostActivity.kt
@@ -0,0 +1,12 @@
+package com.teamtripdraw.android.ui.post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamtripdraw.android.R
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
---
+++ android/app/src/main/java/com/teamtripdraw/android/ui/post/PostViewModel.kt
@@ -0,0 +1,4 @@
+package com.teamtripdraw.android.ui.post
+
+class PostViewModel {
+}
