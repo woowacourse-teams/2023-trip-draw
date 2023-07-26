0841cdbcb3aa52ede926c01f2e509ffa50fc43bc
greeng00se <thegreengoose7@gmail.com>
2023-07-26 09:18:10 +0900

    [refactor] Swagger 설정 builder 패턴 사용하는 일관된 형식으로 변경

--- backend/src/main/java/dev/tripdraw/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/dev/tripdraw/config/swagger/SwaggerConfig.java
@@ -8,34 +8,39 @@ import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        final Info info = createInfo();
-
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
-                .components(
-                        new Components()
-                                .addHeaders("Authorization", createCustomizedHeader()))
-                .info(info);
+                .addServersItem(url())
+                .components(authorization())
+                .info(info());
     }
 
-    private Info createInfo() {
-        final Info info = new Info()
+    private Server url() {
+        return new Server()
+                .url("/");
+    }
+
+    private Components authorization() {
+        return new Components()
+                .addHeaders(HttpHeaders.AUTHORIZATION, authorzationHeader());
+    }
+
+    private Header authorzationHeader() {
+        return new Header()
+                .description("Authorization : BASE64(nickname)")
+                .schema(new StringSchema());
+    }
+
+    private Info info() {
+        return new Info()
                 .title("트립드로우 서버 API 명세서")
                 .version("v0.0.1")
                 .description("API Description");
-        return info;
-    }
-
-    private Header createCustomizedHeader() {
-        Header customHeader = new Header();
-        customHeader.setDescription("Authorization : BASE64(nickname)");
-        customHeader.schema(new StringSchema());
-        return customHeader;
     }
 }
