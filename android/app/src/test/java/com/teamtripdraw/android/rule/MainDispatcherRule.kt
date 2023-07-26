780d75bb315c27e7fbcfa7707a002b8b4d647d6c
pingu244 <pingu244@naver.com>
2023-07-26 15:18:01 +0900

    [test] 닉네임 설정 / viewModel 테스트 작성 (#64)

---
+++ android/app/src/test/java/com/teamtripdraw/android/rule/MainDispatcherRule.kt
@@ -0,0 +1,23 @@
+package com.teamtripdraw.android.rule
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(), // Scheduler를 새로 생성한다.
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
