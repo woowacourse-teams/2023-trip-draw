d51ed4964da0e3cb5cb788795dc431269f4def27
Woojin Lee <dhkep03@gmail.com>
2023-07-27 16:33:46 +0900

    [docs] 요청 객체 필드 설명 추가 (#72)

--- backend/src/main/java/dev/tripdraw/dto/post/PostPointCreateRequest.java
+++ backend/src/main/java/dev/tripdraw/dto/post/PostPointCreateRequest.java
@@ -1,6 +1,10 @@
 package dev.tripdraw.dto.post;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import dev.tripdraw.domain.trip.Point;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
 public record PostPointCreateRequest(
@@ -10,6 +14,8 @@ public record PostPointCreateRequest(
         String writing,
         Double latitude,
         Double longitude,
+        @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+        @Schema(description = "yyyy-MM-dd'T'HH:mm:ss 형식")
         LocalDateTime recordedAt
 ) {
 
