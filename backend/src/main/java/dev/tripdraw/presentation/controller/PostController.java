85a9d205a2f86038f1934a18904464082a02fcf4
Woojin Lee <dhkep03@gmail.com>
2023-07-27 17:05:30 +0900

    [feat] 현재 위치에 대한 감상을 생성하는 API 구현 (#72)

---
+++ backend/src/main/java/dev/tripdraw/presentation/controller/PostController.java
@@ -0,0 +1,38 @@
+package dev.tripdraw.presentation.controller;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+import dev.tripdraw.application.PostService;
+import dev.tripdraw.config.swagger.SwaggerLoginRequired;
+import dev.tripdraw.dto.auth.LoginUser;
+import dev.tripdraw.dto.post.PostPointCreateRequest;
+import dev.tripdraw.dto.post.PostResponse;
+import dev.tripdraw.presentation.member.Auth;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Post", description = "감상 관련 API 명세")
+@RestController
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @SwaggerLoginRequired
+    @Operation(summary = "현재 위치에 대한 감상 생성 API", description = "진행 중인 여행에서, 현재 위치에 대한 감상 생성")
+    @PostMapping("/posts/current-location")
+    public ResponseEntity<PostResponse> createOfCurrentLocation(
+            @Auth LoginUser loginUser,
+            @RequestBody PostPointCreateRequest postPointCreateRequest
+    ) {
+        PostResponse response = postService.createOfCurrentLocation(loginUser, postPointCreateRequest);
+        return ResponseEntity.status(CREATED).body(response);
+    }
+}
