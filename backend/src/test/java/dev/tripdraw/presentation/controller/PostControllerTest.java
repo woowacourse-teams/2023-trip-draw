355c8360ee0214f1b655fb8ea5cb49512647c24d
Woojin Lee <dhkep03@gmail.com>
2023-07-27 17:05:30 +0900

    [feat] 현재 위치에 대한 감상을 생성하는 API 구현 (#72)

---
+++ backend/src/test/java/dev/tripdraw/presentation/controller/PostControllerTest.java
@@ -0,0 +1,126 @@
+package dev.tripdraw.presentation.controller;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import dev.tripdraw.domain.member.Member;
+import dev.tripdraw.domain.member.MemberRepository;
+import dev.tripdraw.domain.trip.Trip;
+import dev.tripdraw.domain.trip.TripRepository;
+import dev.tripdraw.dto.post.PostPointCreateRequest;
+import dev.tripdraw.dto.post.PostResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class PostControllerTest extends ControllerTest {
+
+    private static final String 통후추_BASE64 = "7Ya17ZuE7LaU";
+    private static final String 순후추_BASE64 = "7Iic7ZuE7LaU";
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Trip trip;
+
+    @BeforeEach
+    void setUp() {
+        super.setUp();
+
+        Member member = memberRepository.save(new Member("통후추"));
+        trip = tripRepository.save(Trip.from(member));
+    }
+
+    @Test
+    void 현재_위치에_대한_감상을_생성한다() {
+        // given
+        PostPointCreateRequest postPointCreateRequest = new PostPointCreateRequest(
+                trip.id(),
+                "우도의 바닷가",
+                "제주특별자치도 제주시 애월읍 소길리",
+                "우도에서 땅콩 아이스크림을 먹었다.\\n너무 맛있었다.",
+                1.1,
+                2.2,
+                LocalDateTime.of(2023, 7, 18, 20, 24)
+        );
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", 통후추_BASE64)
+                .body(postPointCreateRequest)
+                .when().post("/posts/current-location")
+                .then().log().all()
+                .extract();
+
+        // then
+        PostResponse postResponse = response.as(PostResponse.class);
+
+        assertSoftly(softly -> {
+            softly.assertThat(response.statusCode()).isEqualTo(CREATED.value());
+            softly.assertThat(postResponse.postId()).isNotNull();
+            softly.assertThat(postResponse.title()).isEqualTo("우도의 바닷가");
+            softly.assertThat(postResponse.pointResponse().pointId()).isNotNull();
+            softly.assertThat(postResponse.pointResponse().latitude()).isEqualTo(1.1);
+        });
+    }
+
+    @Test
+    void 현재_위치에_대한_감상을_생성할_때_인증에_실패하면_예외를_발생시킨다() {
+        // given
+        PostPointCreateRequest postPointCreateRequest = new PostPointCreateRequest(
+                trip.id(),
+                "우도의 바닷가",
+                "제주특별자치도 제주시 애월읍 소길리",
+                "우도에서 땅콩 아이스크림을 먹었다.\\n너무 맛있었다.",
+                1.1,
+                2.2,
+                LocalDateTime.of(2023, 7, 18, 20, 24)
+        );
+
+        // expect
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", 순후추_BASE64)
+                .body(postPointCreateRequest)
+                .when().post("/posts/current-location")
+                .then().log().all()
+                .statusCode(FORBIDDEN.value());
+    }
+
+    @Test
+    void 현재_위치에_대한_감상을_생성할_때_존재하지_않는_여행의_ID이면_예외를_발생시킨다() {
+        // given
+        PostPointCreateRequest postPointCreateRequest = new PostPointCreateRequest(
+                -1L,
+                "우도의 바닷가",
+                "제주특별자치도 제주시 애월읍 소길리",
+                "우도에서 땅콩 아이스크림을 먹었다.\\n너무 맛있었다.",
+                1.1,
+                2.2,
+                LocalDateTime.of(2023, 7, 18, 20, 24)
+        );
+
+        // expect
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", 통후추_BASE64)
+                .body(postPointCreateRequest)
+                .when().post("/posts/current-location")
+                .then().log().all()
+                .statusCode(NOT_FOUND.value());
+    }
+}
