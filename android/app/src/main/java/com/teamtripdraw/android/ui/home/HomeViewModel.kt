bb2343deb982111cbea4fe3a3dc0b7afc3f18651
changhwan <chang5hustle@gmail.com>
2023-07-28 17:49:45 +0900

    [feat] 메인 화면_홈 / 여행시작 로직 버튼에 연결 (#92)

--- android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeViewModel.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeViewModel.kt
@@ -3,7 +3,9 @@ package com.teamtripdraw.android.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.teamtripdraw.android.domain.repository.TripRepository
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val tripRepository: TripRepository
@@ -14,5 +16,11 @@ class HomeViewModel(
 
     init {
         // todo: Local의 trip Id 조사하여 없다면 여행전 업데이트 있다면 여행중 업데이트 로직
+    }
+
+    fun startTrip() {
+        viewModelScope.launch {
+            tripRepository.startTrip()
+        }
     }
 }
