b79b8c4e705dc062ccbe01f8934227018a509b79
changhwan <chang5hustle@gmail.com>
2023-07-28 22:23:42 +0900

    [fix] 닉네임 설정 / 닉네임 조회 Dto,service 생성 (#107)

--- android/app/src/main/java/com/teamtripdraw/android/di/ServiceContainer.kt
+++ android/app/src/main/java/com/teamtripdraw/android/di/ServiceContainer.kt
@@ -1,8 +1,11 @@
 package com.teamtripdraw.android.di
 
+import com.teamtripdraw.android.data.httpClient.service.GetNicknameService
 import com.teamtripdraw.android.data.httpClient.service.NicknameSetupService
 
 class ServiceContainer(retrofitContainer: RetrofitContainer) {
     val nicknameSetupService: NicknameSetupService =
         retrofitContainer.tripDrawRetrofit.create(NicknameSetupService::class.java)
+    val getNicknameService: GetNicknameService =
+        retrofitContainer.tripDrawRetrofit.create(GetNicknameService::class.java)
 }
