9943055e670e4f71517d69ffcf37756bfec91bda
changhwan <chang5hustle@gmail.com>
2023-07-28 16:02:07 +0900

    [feat] 메인 화면_홈 / CreateTripResponse 생성 (#92)

---
+++ android/app/src/main/java/com/teamtripdraw/android/data/httpClient/dto/response/CreateTripResponse.kt
@@ -0,0 +1,11 @@
+package com.teamtripdraw.android.data.httpClient.dto.response
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class CreateTripResponse(
+    @Json(name = "tripId")
+    val tripId: Long
+)
