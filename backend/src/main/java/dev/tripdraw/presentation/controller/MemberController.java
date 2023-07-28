8ebd8482f507faaf127dbf8355ef239b7121c8f2
greeng00se <thegreengoose7@gmail.com>
2023-07-28 22:02:17 +0900

    [feat] 회원 조회 기능 추가 (#105)

--- backend/src/main/java/dev/tripdraw/presentation/controller/MemberController.java
+++ backend/src/main/java/dev/tripdraw/presentation/controller/MemberController.java
@@ -5,11 +5,14 @@ import static org.springframework.http.HttpStatus.CREATED;
 import dev.tripdraw.application.MemberService;
 import dev.tripdraw.dto.member.MemberCreateRequest;
 import dev.tripdraw.dto.member.MemberCreateResponse;
+import dev.tripdraw.dto.member.MemberSearchResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +38,16 @@ public class MemberController {
     public ResponseEntity<MemberCreateResponse> create(@Valid @RequestBody MemberCreateRequest memberCreateRequest) {
         MemberCreateResponse response = memberService.register(memberCreateRequest);
         return ResponseEntity.status(CREATED).body(response);
+    }
+
+    @Operation(summary = "사용자 조회 API", description = "사용자를 조회합니다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "사용자 조회 성공."
+    )
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberSearchResponse> findById(@PathVariable Long memberId) {
+        MemberSearchResponse response = memberService.findById(memberId);
+        return ResponseEntity.ok(response);
     }
 }
