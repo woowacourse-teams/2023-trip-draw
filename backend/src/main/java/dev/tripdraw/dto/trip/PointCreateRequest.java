7476c1f5001f930871e5ff8a92b5de94f1efea66
Woojin Lee <dhkep03@gmail.com>
2023-07-28 11:57:19 +0900

    [chore] swagger 변경사항 충돌 해결 (#72)

--- backend/src/main/java/dev/tripdraw/dto/trip/PointCreateRequest.java
+++ backend/src/main/java/dev/tripdraw/dto/trip/PointCreateRequest.java
@@ -8,11 +8,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
 public record PointCreateRequest(
+        @Schema(description = "여행 Id", example = "1")
         Long tripId,
+
+        @Schema(description = "위도", example = "37.56663888630603")
         Double latitude,
+
+        @Schema(description = "경도", example = "126.97838310403904")
         Double longitude,
+
         @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-        @Schema(description = "yyyy-MM-dd'T'HH:mm:ss 형식")
+        @Schema(description = "yyyy-MM-dd'T'HH:mm:ss 형식", example = "2023-07-23T19:48:27", type = "string")
         LocalDateTime recordedAt
 ) {
 
--- backend/src/main/java/dev/tripdraw/presentation/controller/TripController.java
+++ backend/src/main/java/dev/tripdraw/presentation/controller/TripController.java
@@ -10,6 +10,7 @@ import dev.tripdraw.dto.trip.PointResponse;
 import dev.tripdraw.dto.trip.TripResponse;
 import dev.tripdraw.presentation.member.Auth;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +29,10 @@ public class TripController {
     }
 
     @Operation(summary = "여행 생성 API", description = "현재 로그인한 사용자의 여행을 생성합니다.")
+    @ApiResponse(
+            responseCode = "201",
+            description = "여행 생성 성공."
+    )
     @PostMapping("/trips")
     public ResponseEntity<TripResponse> create(@Auth LoginUser loginUser) {
         TripResponse response = tripService.create(loginUser);
@@ -35,6 +40,10 @@ public class TripController {
     }
 
     @Operation(summary = "위치 정보 저장 API", description = "현재 진행 중인 여행의 경로에 위치 정보를 저장합니다.")
+    @ApiResponse(
+            responseCode = "201",
+            description = "위치 정보 저장 성공."
+    )
     @PostMapping("/points")
     public ResponseEntity<PointResponse> addPoint(
             @Auth LoginUser loginUser,
