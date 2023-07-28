484d09ebc694d8d6a0087f1085719839094628a3
Woojin Lee <dhkep03@gmail.com>
2023-07-28 11:04:55 +0900

    [feat] 사용자가 선택한 위치에서 감상 생성 - service 메서드 구현 (#72)

--- backend/src/test/java/dev/tripdraw/application/PostServiceTest.java
+++ backend/src/test/java/dev/tripdraw/application/PostServiceTest.java
@@ -1,6 +1,7 @@
 package dev.tripdraw.application;
 
 import static dev.tripdraw.exception.member.MemberExceptionType.MEMBER_NOT_FOUND;
+import static dev.tripdraw.exception.trip.TripExceptionType.POINT_NOT_FOUND;
 import static dev.tripdraw.exception.trip.TripExceptionType.TRIP_NOT_FOUND;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -8,10 +9,12 @@ import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import dev.tripdraw.domain.member.Member;
 import dev.tripdraw.domain.member.MemberRepository;
 import dev.tripdraw.domain.post.PostRepository;
+import dev.tripdraw.domain.trip.Point;
 import dev.tripdraw.domain.trip.Trip;
 import dev.tripdraw.domain.trip.TripRepository;
 import dev.tripdraw.dto.auth.LoginUser;
 import dev.tripdraw.dto.post.PostPointCreateRequest;
+import dev.tripdraw.dto.post.PostRequest;
 import dev.tripdraw.dto.post.PostResponse;
 import dev.tripdraw.exception.member.MemberException;
 import dev.tripdraw.exception.trip.TripException;
@@ -37,16 +40,19 @@ class PostServiceTest {
 
     private Trip trip;
     private LoginUser loginUser;
+    private Point point;
 
     @BeforeEach
     void setUp() {
         Member member = memberRepository.save(new Member("통후추"));
         trip = tripRepository.save(Trip.from(member));
+        point = new Point(1.1, 2.1, LocalDateTime.now());
+        trip.add(point);
+        tripRepository.flush();
         loginUser = new LoginUser("통후추");
     }
 
 
-    // 정상 저장
     @Test
     void 현재_위치에_대한_감상을_생성한다() {
         // given
@@ -71,12 +77,11 @@ class PostServiceTest {
         });
     }
 
-    // nickname에 대한 Member 없을 때 예외
     @Test
     void 현재_위치에_대한_감상을_생성할_때_존재하지_않는_사용자_닉네임이면_예외를_발생시킨다() {
         // given
         LoginUser wrongUser = new LoginUser("상한 통후추");
-        PostPointCreateRequest requestOfNotExistedTripId = new PostPointCreateRequest(
+        PostPointCreateRequest postPointCreateRequest = new PostPointCreateRequest(
                 trip.id(),
                 "우도의 바닷가",
                 "제주특별자치도 제주시 애월읍 소길리",
@@ -87,12 +92,11 @@ class PostServiceTest {
         );
 
         // expect
-        assertThatThrownBy(() -> postService.createOfCurrentLocation(wrongUser, requestOfNotExistedTripId))
+        assertThatThrownBy(() -> postService.createOfCurrentLocation(wrongUser, postPointCreateRequest))
                 .isInstanceOf(MemberException.class)
                 .hasMessage(MEMBER_NOT_FOUND.getMessage());
     }
 
-    // tripId로 Trip 조회할 수 없을 때 예외
     @Test
     void 현재_위치에_대한_감상을_생성할_때_존재하지_않는_여행의_ID이면_예외를_발생시킨다() {
         // given
@@ -110,5 +114,79 @@ class PostServiceTest {
         assertThatThrownBy(() -> postService.createOfCurrentLocation(loginUser, requestOfNotExistedTripId))
                 .isInstanceOf(TripException.class)
                 .hasMessage(TRIP_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void 사용자가_선택한_위치에_대한_감상을_생성한다() {
+        // given
+        PostRequest postRequest = new PostRequest(
+                trip.id(),
+                point.id(),
+                "우도의 바닷가",
+                "제주특별자치도 제주시 애월읍 소길리",
+                "우도에서 땅콩 아이스크림을 먹었다.\\n너무 맛있었다."
+        );
+
+        // when
+        PostResponse postResponse = postService.create(loginUser, postRequest);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(postResponse.postId()).isNotNull();
+            softly.assertThat(postResponse.title()).isEqualTo("우도의 바닷가");
+            softly.assertThat(postResponse.pointResponse().pointId()).isNotNull();
+        });
+    }
+
+    @Test
+    void 사용자가_선택한_위치에_대한_감상을_생성할_때_존재하지_않는_사용자_닉네임이면_예외를_발생시킨다() {
+        // given
+        LoginUser wrongUser = new LoginUser("상한 통후추");
+        PostRequest postRequest = new PostRequest(
+                trip.id(),
+                point.id(),
+                "우도의 바닷가",
+                "제주특별자치도 제주시 애월읍 소길리",
+                "우도에서 땅콩 아이스크림을 먹었다.\\n너무 맛있었다."
+        );
+
+        // expect
+        assertThatThrownBy(() -> postService.create(wrongUser, postRequest))
+                .isInstanceOf(MemberException.class)
+                .hasMessage(MEMBER_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void 사용자가_선택한_위치에_대한_감상을_생성할_때_존재하지_않는_여행의_ID이면_예외를_발생시킨다() {
+        // given
+        PostRequest requestOfNotExistedTripId = new PostRequest(
+                -1L,
+                point.id(),
+                "우도의 바닷가",
+                "제주특별자치도 제주시 애월읍 소길리",
+                "우도에서 땅콩 아이스크림을 먹었다.\\n너무 맛있었다."
+        );
+
+        // expect
+        assertThatThrownBy(() -> postService.create(loginUser, requestOfNotExistedTripId))
+                .isInstanceOf(TripException.class)
+                .hasMessage(TRIP_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void 사용자가_선택한_위치에_대한_감상을_생성할_때_존재하지_않는_위치의_ID이면_예외를_발생시킨다() {
+        // given
+        PostRequest requestOfNotExistedPointId = new PostRequest(
+                trip.id(),
+                -1L,
+                "우도의 바닷가",
+                "제주특별자치도 제주시 애월읍 소길리",
+                "우도에서 땅콩 아이스크림을 먹었다.\\n너무 맛있었다."
+        );
+
+        // expect
+        assertThatThrownBy(() -> postService.create(loginUser, requestOfNotExistedPointId))
+                .isInstanceOf(TripException.class)
+                .hasMessage(POINT_NOT_FOUND.getMessage());
     }
 }
