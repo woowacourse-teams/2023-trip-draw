08cc14de6f07bac1e2c7ce7159d5db6f0a789877
changhwan <chang5hustle@gmail.com>
2023-07-28 16:05:49 +0900

    [feat] 메인 화면_홈 / CreateTripService 생성 (#92)

--- android/app/src/main/java/com/teamtripdraw/android/di/ServiceContainer.kt
+++ android/app/src/main/java/com/teamtripdraw/android/di/ServiceContainer.kt
@@ -1,8 +1,12 @@
 package com.teamtripdraw.android.di
 
+import com.teamtripdraw.android.data.httpClient.service.CreateTripService
 import com.teamtripdraw.android.data.httpClient.service.NicknameSetupService
+import retrofit2.create
 
 class ServiceContainer(retrofitContainer: RetrofitContainer) {
     val nicknameSetupService: NicknameSetupService =
         retrofitContainer.tripDrawRetrofit.create(NicknameSetupService::class.java)
+    val createTripService: CreateTripService =
+        retrofitContainer.tripDrawRetrofit.create(CreateTripService::class.java)
 }
