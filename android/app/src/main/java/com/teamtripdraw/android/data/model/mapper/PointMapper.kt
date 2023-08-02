cafbc914771fd9fdefb8ec7c8c89e6f8dd93d126
changhwan <chang5hustle@gmail.com>
2023-08-02 17:01:49 +0900

    [feat] 메인 화면_홈 / 위치저장 서버통신 작업 (#149)

--- android/app/src/main/java/com/teamtripdraw/android/data/model/mapper/PointMapper.kt
+++ android/app/src/main/java/com/teamtripdraw/android/data/model/mapper/PointMapper.kt
@@ -2,12 +2,12 @@ package com.teamtripdraw.android.data.model.mapper
 
 import com.teamtripdraw.android.data.model.DataPrePoint
 import com.teamtripdraw.android.domain.model.point.PrePoint
-import com.teamtripdraw.android.support.framework.presentation.LocalDateTimeFormatter.isoLocalDateTimeFormatter
+import com.teamtripdraw.android.support.framework.presentation.LocalDateTimeFormatter.isoRemoveNanoSecondFormatter
 
 fun PrePoint.toData(): DataPrePoint =
     DataPrePoint(
         latitude = latitude,
         longitude = longitude,
-        recordedAt = recordedAt.format(isoLocalDateTimeFormatter)
+        recordedAt = recordedAt.format(isoRemoveNanoSecondFormatter)
     )
 
