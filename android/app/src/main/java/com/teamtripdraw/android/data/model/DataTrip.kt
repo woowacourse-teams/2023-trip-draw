b0e43c110a75743be06361705ac6f5c343ce6357
changhwan <chang5hustle@gmail.com>
2023-08-03 11:43:45 +0900

    [feat] 메인 화면_홈 / 데이터 모델 생성 (#166)

---
+++ android/app/src/main/java/com/teamtripdraw/android/data/model/DataTrip.kt
@@ -0,0 +1,10 @@
+package com.teamtripdraw.android.data.model
+
+data class DataTrip(
+    val tripId: Long,
+    val name: String,
+    val route: List<DataPoint>,
+    val status: String,
+    val imageUrl: String,
+    val routeImageUrl: String
+)
