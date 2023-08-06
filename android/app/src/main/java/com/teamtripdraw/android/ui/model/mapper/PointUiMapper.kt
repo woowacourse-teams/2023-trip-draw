feba3764d8aa5806dd58c7943cb90fb8352b57bc
changhwan <chang5hustle@gmail.com>
2023-08-06 12:56:50 +0900

    [feat] 메인 화면_홈 / 컨벤션 관련  mapper 네이밍 변경 (#200)

--- android/app/src/main/java/com/teamtripdraw/android/ui/model/mapper/PointUiMapper.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/model/mapper/PointUiMapper.kt
@@ -3,7 +3,7 @@ package com.teamtripdraw.android.ui.model.mapper
 import com.teamtripdraw.android.domain.model.point.Point
 import com.teamtripdraw.android.ui.model.UiPoint
 
-fun Point.toUi(): UiPoint =
+fun Point.toPresentation(): UiPoint =
     UiPoint(
         pointId = pointId,
         latitude = latitude,
