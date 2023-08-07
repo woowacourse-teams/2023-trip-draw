90d5be3ae1f32d57b0b52b174abd42f97917b239
changhwan <chang5hustle@gmail.com>
2023-08-08 00:06:01 +0900

    [refactor] 메인 화면_홈 / 뷰모델에서 도메인값 관리하며 노출은 Ui모델이 되도록 변경 (#200)

--- android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeViewModel.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/home/HomeViewModel.kt
@@ -2,11 +2,13 @@ package com.teamtripdraw.android.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.location.LocationResult
 import com.teamtripdraw.android.domain.constants.NULL_SUBSTITUTE_TRIP_ID
 import com.teamtripdraw.android.domain.model.point.PrePoint
+import com.teamtripdraw.android.domain.model.point.Route
 import com.teamtripdraw.android.domain.repository.PointRepository
 import com.teamtripdraw.android.domain.repository.TripRepository
 import com.teamtripdraw.android.support.framework.presentation.event.Event
@@ -26,8 +28,9 @@ class HomeViewModel(
     private val _startTripEvent = MutableLiveData<Event<Boolean>>()
     val startTripEvent: LiveData<Event<Boolean>> = _startTripEvent
 
-    private val _currentTripRoute = MutableLiveData<UiRoute>()
-    val currentTripRoute: LiveData<UiRoute> = _currentTripRoute
+    private val _currentTripRoute = MutableLiveData<Route>()
+    val currentTripUiRoute: LiveData<UiRoute> =
+        Transformations.map(_currentTripRoute) { route -> route.toPresentation() }
 
     private val _markerViewModeState = MutableLiveData<Boolean>(false)
     val markerViewModeState: LiveData<Boolean> = _markerViewModeState
@@ -77,7 +80,7 @@ class HomeViewModel(
         viewModelScope.launch {
             tripRepository.getCurrentTripRoute(tripId)
                 .onSuccess {
-                    _currentTripRoute.value = it.toPresentation()
+                    _currentTripRoute.value = it
                 }
         }
     }
