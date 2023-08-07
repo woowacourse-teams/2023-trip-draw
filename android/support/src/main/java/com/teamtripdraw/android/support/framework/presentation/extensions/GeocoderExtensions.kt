a739c4c9254ae51e999d3c3930dc08015fcc3b8f
otter <koreanotter6@gmail.com>
2023-08-07 18:09:20 +0900

    [refactor] 감상 기록 / 중복 코드 제거 (#211)

--- android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/extensions/GeocoderExtensions.kt
+++ android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/extensions/GeocoderExtensions.kt
@@ -4,47 +4,36 @@ import android.location.Address
 import android.location.Geocoder
 import android.os.Build
 
-fun Geocoder.fetchAdministrativeAddress(
+fun Geocoder.fetchAddress(
     latitude: Double, longitude: Double, event: (address: String) -> Unit
 ) {
-
-    val MAX_ADDRESSES_RESULT_NUMBER = 1
-    val defaultAdministrativeArea = "x: $latitude, y: $longitude"
+    val ADDRESSES_RESULT_MAX_NUMBER = 1
+    val defaultAddress = "x: $latitude, y: $longitude"
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { // API 레벨 33 이상
         getFromLocation(
-            latitude, longitude, MAX_ADDRESSES_RESULT_NUMBER
+            latitude, longitude, ADDRESSES_RESULT_MAX_NUMBER
         ) { addresses: List<Address> ->
-            if (addresses.isEmpty() || addresses[0].getAddressLine(0).isEmpty()) {
-                event(defaultAdministrativeArea)
-                return@getFromLocation
-            }
-
-            val fullAddress = addresses[0].getAddressLine(0)
-            val addressParts: MutableList<String> = fullAddress.split(" ").toMutableList()
-            addressParts.removeFirst()
-            addressParts.removeLast()
-            val addressText = addressParts.joinToString(" ")
-
-            if (addressParts.isNotEmpty()) event(addressText)
-            else event(defaultAdministrativeArea)
+            event(getAdministrativeAddress(addresses, defaultAddress))
         }
     } else { // API 레벨 33 미만
         val addresses: List<Address>? =
-            getFromLocation(latitude, longitude, MAX_ADDRESSES_RESULT_NUMBER)
-
-        if (addresses == null || addresses.isEmpty() || addresses[0].getAddressLine(0).isEmpty()) {
-            event(defaultAdministrativeArea)
-            return
-        }
-
-        val fullAddress = addresses[0].getAddressLine(0)
-        val addressParts: MutableList<String> = fullAddress.split(" ").toMutableList()
-        addressParts.removeFirst()
-        addressParts.removeLast()
-        val addressText = addressParts.joinToString(" ")
-
-        if (addressParts.isNotEmpty()) event(addressText)
-        else event(defaultAdministrativeArea)
+            getFromLocation(latitude, longitude, ADDRESSES_RESULT_MAX_NUMBER)
+        getAdministrativeAddress(addresses, defaultAddress)
     }
+}
+
+private fun getAdministrativeAddress(addresses: List<Address>?, defaultAddress: String): String {
+    if (addresses == null || addresses.isEmpty() || addresses[0].getAddressLine(0).isEmpty()) {
+        return defaultAddress
+    }
+
+    val fullAddress = addresses[0].getAddressLine(0)
+    val addressParts: MutableList<String> = fullAddress.split(" ").toMutableList()
+    addressParts.removeFirst()
+    addressParts.removeLast()
+    val address = addressParts.joinToString(" ")
+
+    return if (addressParts.isNotEmpty()) address
+    else defaultAddress
 }
