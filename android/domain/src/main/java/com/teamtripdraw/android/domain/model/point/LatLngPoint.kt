ed11d02b49940edf5b23ada953c156c421323ea2
otter <koreanotter6@gmail.com>
2023-08-07 16:18:05 +0900

    [refactor] 감상 기록 / geocoder 사용 방식 변경 (#211)

--- android/domain/src/main/java/com/teamtripdraw/android/domain/model/point/LatLngPoint.kt
+++
@@ -1,6 +0,0 @@
-package com.teamtripdraw.android.domain.model.point
-
-data class LatLngPoint(
-    val latitude: Double,
-    val longitude: Double
-)
--- android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/extensions/GeocoderExtensions.kt
+++ android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/extensions/GeocoderExtensions.kt
@@ -4,29 +4,45 @@ import android.location.Address
 import android.location.Geocoder
 import android.os.Build
 
-fun Geocoder.getAdministrativeAddress(latitude: Double, longitude: Double): String {
+fun Geocoder.fetchAdministrativeAddress(
+    latitude: Double, longitude: Double, event: (address: String) -> Unit
+) {
+
     val MAX_ADDRESSES_RESULT_NUMBER = 1
-    var administrativeArea: String = "x: $latitude, y: $longitude"
+    val defaultAdministrativeArea = "x: $latitude, y: $longitude"
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { // API 레벨 33 이상
-        getFromLocation(latitude, longitude, MAX_ADDRESSES_RESULT_NUMBER) { addresses: List<Address> ->
-            if (addresses.isNotEmpty()
-                && addresses[0].getAddressLine(0).isNotEmpty()
-            ) {
-                val address = addresses[0]
-                administrativeArea = address.adminArea + address.subAdminArea
+        getFromLocation(
+            latitude, longitude, MAX_ADDRESSES_RESULT_NUMBER
+        ) { addresses: List<Address> ->
+            if (addresses.isEmpty() || addresses[0].getAddressLine(0).isEmpty()) {
+                event(defaultAdministrativeArea)
+                return@getFromLocation
             }
+
+            val address = addresses[0]
+            val adminArea: String = address.adminArea ?: ""
+            val subAdminArea: String = address.featureName ?: ""
+            val administrativeArea = adminArea + subAdminArea
+
+            if (administrativeArea != "") event(administrativeArea)
+            else event(defaultAdministrativeArea)
         }
     } else { // API 레벨 33 미만
-        val addresses: List<Address>? = getFromLocation(latitude, longitude, 1)
-        if (addresses != null
-            && addresses.isNotEmpty()
-            && addresses[0].getAddressLine(0).isNotEmpty()
-        ) {
-            val address = addresses[0]
-            administrativeArea = address.adminArea + address.subAdminArea
-        }
-    }
+        val addresses: List<Address>? =
+            getFromLocation(latitude, longitude, MAX_ADDRESSES_RESULT_NUMBER)
 
-    return administrativeArea
+        if (addresses == null || addresses.isEmpty() || addresses[0].getAddressLine(0).isEmpty()) {
+            event(defaultAdministrativeArea)
+            return
+        }
+
+        val address = addresses[0]
+        val adminArea: String = address.adminArea ?: ""
+        val subAdminArea: String = address.subAdminArea ?: ""
+        val administrativeArea = adminArea + subAdminArea
+
+        if (administrativeArea != "") event(administrativeArea)
+        else event(defaultAdministrativeArea)
+    }
 }
