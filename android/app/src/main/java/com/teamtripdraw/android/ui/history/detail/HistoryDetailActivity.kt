4c05cbb85acd4b0a7029f71b71bfecf87b5a9348
pingu244 <pingu244@naver.com>
2023-08-08 10:32:36 +0900

    [ui] 여행 히스토리 / 나의 여행 상세 화면 (#212)

---
+++ android/app/src/main/java/com/teamtripdraw/android/ui/history/detail/HistoryDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.teamtripdraw.android.ui.history.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamtripdraw.android.R
+
+class HistoryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_detail)
+    }
+}
