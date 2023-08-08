a0e7c1322618ee2a563ce50c434a83f75ec23c67
otter <koreanotter6@gmail.com>
2023-08-09 00:16:29 +0900

    [feat] 감상 기록 / 사진 리사이즈 및 하위 버전 지원 기능 추가 (#219)

--- android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/extensions/UriExtensions.kt
+++ android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/extensions/UriExtensions.kt
@@ -1,20 +1,70 @@
 package com.teamtripdraw.android.support.framework.presentation.extensions
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.ExifInterface
 import android.net.Uri
-import android.provider.MediaStore
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
 
-fun Uri.toFile(context: Context): File? {
-    val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
-    val contentResolver = context.contentResolver
-    val cursor = contentResolver.query(this, filePathColumn, null, null, null)
-    cursor?.use {
-        if (it.moveToFirst()) {
-            val columnIndex = it.getColumnIndex(filePathColumn[0])
-            val filePath = it.getString(columnIndex)
-            return File(filePath)
+fun Uri.toResizedImageFile(context: Context): File? {
+    val bitmap = toBitmap(context) ?: return null
+    val file = getResizedImageFile(bitmap, context.cacheDir)
+    val orientation = context.contentResolver
+        .openInputStream(this)?.use {
+            ExifInterface(it)
+        }?.getAttribute(ExifInterface.TAG_ORIENTATION)
+
+    orientation?.let { file.setOrientation(it) }
+
+    return file
+}
+
+private fun Uri.toBitmap(context: Context): Bitmap? {
+    return context.contentResolver
+        .openInputStream(this)?.use {
+            BitmapFactory.decodeStream(it)
         }
+}
+
+private fun getResizedImageFile(bitmap: Bitmap, directory: File): File {
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    val resizedBitmap: Bitmap = getResizedBitmap(bitmap)
+    resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+
+    val tempFile = File.createTempFile("image", ".jpg", directory)
+    FileOutputStream(tempFile).use {
+        it.write(byteArrayOutputStream.toByteArray())
     }
-    return null
+    return tempFile
+}
+
+private fun getResizedBitmap(bitmap: Bitmap): Bitmap {
+    val RESIZE_SIZE = 720f
+    var resizedWidth: Float = RESIZE_SIZE
+    var resizedHeight: Float = RESIZE_SIZE
+
+    if (bitmap.width >= RESIZE_SIZE && bitmap.height >= RESIZE_SIZE) {
+        if (bitmap.width >= bitmap.height) {
+            resizedWidth = RESIZE_SIZE * (bitmap.width.toFloat() / bitmap.height.toFloat())
+        } else {
+            resizedHeight = RESIZE_SIZE * (bitmap.height.toFloat() / bitmap.width.toFloat())
+        }
+    } else {
+        resizedWidth = bitmap.width.toFloat()
+        resizedHeight = bitmap.height.toFloat()
+    }
+
+    return Bitmap.createScaledBitmap(
+        bitmap, resizedWidth.toInt(), resizedHeight.toInt(), true
+    )
+}
+
+private fun File.setOrientation(orientation: String) {
+    ExifInterface(this.path).apply {
+        setAttribute(ExifInterface.TAG_ORIENTATION, orientation)
+        saveAttributes()
+    }
 }
