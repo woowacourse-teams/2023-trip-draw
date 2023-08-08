5394a503898b7ec7d4da9f7df526c3138e955e1e
changhwan <chang5hustle@gmail.com>
2023-08-08 15:08:03 +0900

    [feat] 메인 화면_홈 / 바텀시트 기초 코드 작업 (#218)

--- android/app/src/main/java/com/teamtripdraw/android/ui/home/MarkerSelectedBottomSheet.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/home/MarkerSelectedBottomSheet.kt
@@ -1,4 +1,30 @@
 package com.teamtripdraw.android.ui.home
 
-class MarkerSelectedBottomSheet {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.teamtripdraw.android.R
+import com.teamtripdraw.android.databinding.BottomSheetMarkerSelectedBinding
+
+class MarkerSelectedBottomSheet : BottomSheetDialogFragment() {
+
+    private var _binding: BottomSheetMarkerSelectedBinding? = null
+    private val binding get() = _binding!!
+
+    private val homeViewModel: HomeViewModel by viewModels({ requireParentFragment() })
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = BottomSheetMarkerSelectedBinding.inflate(layoutInflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun getTheme(): Int = R.style.RoundBottomSheetStyleTheme
 }
