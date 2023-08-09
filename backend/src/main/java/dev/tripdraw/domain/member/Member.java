613729cc3a0fe391156565ec4698adf1cca2bfe0
Combi153 <jcm04@naver.com>
2023-08-09 21:44:08 +0900

    [feat] Member isDeleted 필드 추가 (#240)

--- backend/src/main/java/dev/tripdraw/domain/member/Member.java
+++ backend/src/main/java/dev/tripdraw/domain/member/Member.java
@@ -1,16 +1,22 @@
 package dev.tripdraw.domain.member;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 import dev.tripdraw.domain.common.BaseEntity;
 import dev.tripdraw.domain.oauth.OauthType;
+import dev.tripdraw.exception.member.MemberException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.Where;
 
+import static dev.tripdraw.exception.member.MemberExceptionType.MEMBER_NOT_FOUND;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+@Where(clause = "is_deleted = FALSE")
 @Entity
 public class Member extends BaseEntity {
 
@@ -27,6 +33,9 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private OauthType oauthType;
+
+    @Column(nullable = false)
+    private Boolean isDeleted = FALSE;
 
     protected Member() {
     }
@@ -46,6 +55,17 @@ public class Member extends BaseEntity {
         return new Member(null, null, oauthId, oauthType);
     }
 
+    public void delete() {
+        validateNotDeleted();
+        this.isDeleted = TRUE;
+    }
+
+    private void validateNotDeleted() {
+        if (isDeleted == TRUE) {
+            throw new MemberException(MEMBER_NOT_FOUND);
+        }
+    }
+
     public void changeNickname(String nickname) {
         this.nickname = nickname;
     }
@@ -56,5 +76,17 @@ public class Member extends BaseEntity {
 
     public String nickname() {
         return nickname;
+    }
+
+    public String oauthId() {
+        return oauthId;
+    }
+
+    public OauthType oauthType() {
+        return oauthType;
+    }
+
+    public Boolean isDeleted() {
+        return isDeleted;
     }
 }
