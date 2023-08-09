c66bc9caf15268d1770d669c323bd3780d57facd
Combi153 <jcm04@naver.com>
2023-08-09 18:38:02 +0900

    [fix] sql vendor 사에 따른 스크립트 분리 (#236)

--- backend/src/main/resources/db/migration/mysql/V4__addOAuth.sql
+++ backend/src/main/resources/db/migration/mysql/V4__addOAuth.sql
@@ -1,3 +1,3 @@
 alter table member add column oauth_id varchar(255);
 alter table member add column oauth_type varchar(255);
-alter table member alter nickname drop not null;
+alter table member modify nickname varchar(255);
