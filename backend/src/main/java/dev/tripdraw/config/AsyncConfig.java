115bec6ff6bc1923ed40b2f1c46d8cc92b28ca6d
greeng00se <thegreengoose7@gmail.com>
2023-08-10 23:32:23 +0900

    [feat] Executor 빈 추가 (#232)

--- backend/src/main/java/dev/tripdraw/config/AsyncConfig.java
+++ backend/src/main/java/dev/tripdraw/config/AsyncConfig.java
@@ -1,9 +1,27 @@
 package dev.tripdraw.config;
 
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig {
+
+    private static final int CORE_POOL_SIZE = 3;
+    private static final int MAX_POOL_SIZE = 10;
+    private static final int QUEUE_CAPACITY = 100;
+    private static final String THREAD_NAME = "Async Task Executor-";
+
+    @Bean
+    public Executor asyncTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(CORE_POOL_SIZE);
+        taskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
+        taskExecutor.setQueueCapacity(QUEUE_CAPACITY);
+        taskExecutor.setThreadNamePrefix(THREAD_NAME);
+        return taskExecutor;
+    }
 }
