4e0d5d09f1a191a12303912aedfdbe0b660204ef
changhwan <chang5hustle@gmail.com>
2023-08-11 00:18:27 +0900

    [feat] 소셜로그인 / 로그인 화면 xml 작업 (#266)

---
+++ android/app/src/main/java/com/teamtripdraw/android/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.teamtripdraw.android.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamtripdraw.android.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
