fdb89257e5926ff527615a3e9abd12b2361331dd
Combi153 <jcm04@naver.com>
2023-08-11 02:55:46 +0900

    [test] 학습 테스트 삭제 (#255)

--- backend/src/test/java/dev/tripdraw/domain/member/MemberRepositoryTest.java
+++ backend/src/test/java/dev/tripdraw/domain/member/MemberRepositoryTest.java
@@ -2,7 +2,6 @@ package dev.tripdraw.domain.member;
 
 import static dev.tripdraw.domain.oauth.OauthType.KAKAO;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -39,41 +38,5 @@ class MemberRepositoryTest {
 
         // expect
         assertThat(foundMember).isEmpty();
-    }
-
-    @Test
-    void ID를_입력_받아_사용자를_삭제한다() {
-        // given
-        Member member = memberRepository.save(new Member("통후추", "kakaoId", KAKAO));
-
-        // when
-        memberRepository.deleteById(member.id());
-
-        // then
-        assertThat(memberRepository.findById(member.id())).isEmpty();
-    }
-
-    @Test
-    void ID를_입력_받아_사용자를_삭제할_때_존재하지_않는_사용자_ID여도_예외가_발생하지_않는다() {
-        // expect
-        assertThatNoException().isThrownBy(() -> memberRepository.deleteById(Long.MIN_VALUE));
-    }
-
-    @Test
-    void 사용자를_입력_받아_삭제한다() {
-        // given
-        Member member = memberRepository.save(new Member("통후추", "kakaoId", KAKAO));
-
-        // when
-        memberRepository.delete(member);
-
-        // then
-        assertThat(memberRepository.findById(member.id())).isEmpty();
-    }
-
-    @Test
-    void 사용자를_입력_받아_삭제할_때_존재하지_않는_사용자여도_예외가_발생하지_않는다() {
-        // expect
-        assertThatNoException().isThrownBy(() -> memberRepository.delete(new Member("", "", KAKAO)));
     }
 }
