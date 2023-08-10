b59c5e8925b97f182a6601cff58fc7502c63b210
Woojin Lee <dhkep03@gmail.com>
2023-08-10 15:39:16 +0900

    [docs] 감상 생성, 수정 API에 multipart/form-data 적용 (#249)

--- backend/src/main/java/dev/tripdraw/presentation/controller/PostController.java
+++ backend/src/main/java/dev/tripdraw/presentation/controller/PostController.java
@@ -2,6 +2,7 @@ package dev.tripdraw.presentation.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import dev.tripdraw.application.PostService;
 import dev.tripdraw.config.swagger.SwaggerAuthorizationRequired;
@@ -14,15 +15,18 @@ import dev.tripdraw.dto.post.PostUpdateRequest;
 import dev.tripdraw.dto.post.PostsResponse;
 import dev.tripdraw.presentation.member.Auth;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -43,11 +47,22 @@ public class PostController {
             responseCode = "201",
             description = "현재 위치에 대한 감상 생성 성공."
     )
-    @PostMapping("/posts/current-location")
+    @PostMapping(
+            path = "/posts/current-location",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<PostCreateResponse> createOfCurrentLocation(
             @Auth LoginUser loginUser,
-            @Valid @RequestPart(value = "dto") PostAndPointCreateRequest postAndPointCreateRequest,
-            @RequestPart(value = "file", required = false) MultipartFile file
+
+            @Parameter(description = "감상 정보를 담은 JSON 객체")
+            @Valid
+            @RequestPart(value = "dto")
+            PostAndPointCreateRequest postAndPointCreateRequest,
+
+            @Parameter(description = "감상에 등록할 이미지 파일")
+            @RequestPart(value = "file", required = false)
+            MultipartFile file
     ) {
         PostCreateResponse response = postService.addAtCurrentPoint(loginUser, postAndPointCreateRequest, file);
         return ResponseEntity.status(CREATED).body(response);
@@ -58,11 +73,23 @@ public class PostController {
             responseCode = "201",
             description = "사용자가 선택한 위치에 대한 감상 생성 성공."
     )
-    @PostMapping("/posts")
+    @PostMapping(
+            path = "/posts",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<PostCreateResponse> create(
-            @Auth LoginUser loginUser,
-            @Valid @RequestPart(value = "dto") PostRequest postRequest,
-            @RequestPart(value = "file", required = false) MultipartFile file
+            @Auth
+            LoginUser loginUser,
+
+            @Parameter(description = "감상 정보를 담은 JSON 객체")
+            @Valid
+            @RequestPart(value = "dto")
+            PostRequest postRequest,
+
+            @Parameter(description = "감상에 등록할 이미지 파일")
+            @RequestParam(value = "file", required = false)
+            MultipartFile file
     ) {
         PostCreateResponse response = postService.addAtExistingLocation(loginUser, postRequest, file);
         return ResponseEntity.status(CREATED).body(response);
@@ -101,11 +128,21 @@ public class PostController {
             responseCode = "204",
             description = "감상 수정 성공."
     )
-    @PatchMapping("/posts/{postId}")
+    @PatchMapping(
+            path = "/posts/{postId}",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
     public ResponseEntity<Void> update(
-            @Auth LoginUser loginUser,
+            @Auth
+            LoginUser loginUser,
+
             @PathVariable Long postId,
-            @Valid @RequestPart(value = "dto") PostUpdateRequest postUpdateRequest,
+
+            @Parameter(description = "수정할 감상 정보를 담은 JSON 객체")
+            @Valid @RequestPart(value = "dto")
+            PostUpdateRequest postUpdateRequest,
+
+            @Parameter(description = "감상에 등록할 이미지 파일")
             @RequestPart(value = "file", required = false) MultipartFile file
     ) {
         postService.update(loginUser, postId, postUpdateRequest, file);
