e166ed9fe35e0d496fa36fd093ada2fdf6bad6ba
changhwan <chang5hustle@gmail.com>
2023-08-11 13:15:32 +0900

    [feat] 소셜로그인 / 닉네임 가져오는 로직 UserInfo 가져오도록 수정 (#266)

--- android/app/src/main/java/com/teamtripdraw/android/data/dataSource/signUp/SignUpDataSource.kt
+++ android/app/src/main/java/com/teamtripdraw/android/data/dataSource/signUp/SignUpDataSource.kt
@@ -1,9 +1,11 @@
 package com.teamtripdraw.android.data.dataSource.signUp
 
+import com.teamtripdraw.android.data.model.DataUserInfo
+
 interface SignUpDataSource {
     interface Local
     interface Remote {
         suspend fun setNickname(nickname: String): Result<Long>
-        suspend fun getUserInfo(accessToken: String): Result<String>
+        suspend fun getUserInfo(accessToken: String): Result<DataUserInfo>
     }
 }
