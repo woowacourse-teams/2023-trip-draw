ca169d33fa01051290140702a44ae1423dfbda76
Combi153 <jcm04@naver.com>
2023-08-11 16:23:05 +0900

    [refactor] 회원 조회 응답 변경 (#255)

--- backend/src/main/java/dev/tripdraw/dto/member/MemberSearchResponse.java
+++ backend/src/main/java/dev/tripdraw/dto/member/MemberSearchResponse.java
@@ -2,6 +2,7 @@ package dev.tripdraw.dto.member;
 
 import dev.tripdraw.domain.member.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Objects;
 
 public record MemberSearchResponse(
         @Schema(description = "사용자 Id", example = "1")
@@ -11,7 +12,9 @@ public record MemberSearchResponse(
         String nickname
 ) {
 
+    private static final String EMPTY_NICKNAME = "";
+
     public static MemberSearchResponse from(Member member) {
-        return new MemberSearchResponse(member.id(), member.nickname());
+        return new MemberSearchResponse(member.id(), Objects.requireNonNullElse(member.nickname(), EMPTY_NICKNAME));
     }
 }
