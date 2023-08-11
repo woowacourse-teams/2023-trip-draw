484b09b41033370c8cbc83fdcdc480ec0100d9c1
changhwan <chang5hustle@gmail.com>
2023-08-11 18:18:19 +0900

    [feat] 소셜로그인 / 닉네임 설정 뷰모델,액티비티 로직 수정 (#266)

--- android/app/src/main/java/com/teamtripdraw/android/ui/signUp/NicknameSetupViewModel.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/signUp/NicknameSetupViewModel.kt
@@ -3,9 +3,15 @@ package com.teamtripdraw.android.ui.signUp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamtripdraw.android.domain.exception.DuplicateNickNameException
+import com.teamtripdraw.android.domain.model.auth.LoginInfo
 import com.teamtripdraw.android.domain.model.user.NicknameValidState
 import com.teamtripdraw.android.domain.repository.AuthRepository
 import com.teamtripdraw.android.support.framework.presentation.event.Event
+import com.teamtripdraw.android.ui.model.UiLoginInfo
+import com.teamtripdraw.android.ui.model.mapper.toDomain
+import kotlinx.coroutines.launch
 
 class NicknameSetupViewModel(
     private val authRepository: AuthRepository,
@@ -23,24 +29,27 @@ class NicknameSetupViewModel(
     private val _nicknameSetupCompletedEvent = MutableLiveData<Event<Boolean>>()
     val nicknameSetupCompleteEvent: LiveData<Event<Boolean>> = _nicknameSetupCompletedEvent
 
+    private lateinit var loginInfo: LoginInfo
+
     fun nicknameChangedEvent() {
         _nicknameState.value = NicknameValidState.getValidState(requireNotNull(nickname.value))
     }
 
     fun setNickname() {
-//        viewModelScope.launch {
-//            authRepository.setNickname(requireNotNull(nickname.value))
-//                .onSuccess {
-//                    _nicknameSetupCompletedEvent.value = Event(true)
-//                }
-//                .onFailure {
-//                    when (it) {
-//                        is DuplicateNickNameException ->
-//                            _nicknameState.value = NicknameValidState.DUPLICATE
-//                    }
-//                }.getOrNull()?.let {
-//                    authRepository.getNickname(it)
-//                }
-//        }
+        viewModelScope.launch {
+            authRepository.setNickname(requireNotNull(nickname.value), loginInfo)
+                .onSuccess {
+                    _nicknameSetupCompletedEvent.value = Event(true)
+                }.onFailure {
+                    when (it) {
+                        is DuplicateNickNameException ->
+                            _nicknameState.value = NicknameValidState.DUPLICATE
+                    }
+                }
+        }
+    }
+
+    fun initLoginInfo(uiLoginInfo: UiLoginInfo) {
+        loginInfo = uiLoginInfo.toDomain()
     }
 }
