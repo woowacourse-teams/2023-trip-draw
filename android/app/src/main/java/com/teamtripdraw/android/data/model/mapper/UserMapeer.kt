7213a02bb4d7dc99040298864488cfb251d8da5c
changhwan <chang5hustle@gmail.com>
2023-08-11 13:04:16 +0900

    [feat] 소셜로그인 / UserInfo Model 관계 정리  (#266)

---
+++ android/app/src/main/java/com/teamtripdraw/android/data/model/mapper/UserMapeer.kt
@@ -0,0 +1,10 @@
+package com.teamtripdraw.android.data.model.mapper
+
+import com.teamtripdraw.android.data.model.DataUserInfo
+import com.teamtripdraw.android.domain.model.user.UserInfo
+
+fun DataUserInfo.toDomain(): UserInfo =
+    UserInfo(
+        memberId = memberId,
+        nickName = nickName,
+    )
