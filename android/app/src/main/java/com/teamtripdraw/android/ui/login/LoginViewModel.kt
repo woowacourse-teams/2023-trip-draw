17ec449b1ff73f668ac3cce850ae59677c8df0a2
changhwan <chang5hustle@gmail.com>
2023-08-11 11:48:27 +0900

    [feat] 소셜로그인 / 로그인 뷰모델,액티비티 로그인 로직 작성(#266)

--- android/app/src/main/java/com/teamtripdraw/android/ui/login/LoginViewModel.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/login/LoginViewModel.kt
@@ -1,5 +1,48 @@
 package com.teamtripdraw.android.ui.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamtripdraw.android.domain.model.auth.LoginInfo
+import com.teamtripdraw.android.domain.model.auth.LoginPlatform
+import com.teamtripdraw.android.domain.repository.AuthRepository
+import com.teamtripdraw.android.support.framework.presentation.event.Event
+import kotlinx.coroutines.launch
 
-class LoginViewModel : ViewModel()
+class LoginViewModel(
+    private val authRepository: AuthRepository,
+) : ViewModel() {
+    private val _kakaoLoginEvent = MutableLiveData<Boolean>(false)
+    val kakaoLoginEvent: LiveData<Boolean> = _kakaoLoginEvent
+
+    private val _existedUserEvent = MutableLiveData<Event<LoginInfo>>()
+    val existedUserEvent: LiveData<Event<LoginInfo>> = _existedUserEvent
+
+    private val _newUserEvent = MutableLiveData<Event<LoginInfo>>()
+    val newUserEvent: LiveData<Event<LoginInfo>> = _newUserEvent
+
+    fun startKakaoLogin() {
+        _kakaoLoginEvent.value = true
+    }
+
+    fun resetKakaoLoginEvent() {
+        _kakaoLoginEvent.value = false
+    }
+
+    fun login(platform: LoginPlatform, socialToken: String) {
+        val loginInfo = LoginInfo(platform, socialToken)
+        viewModelScope.launch {
+            authRepository.login(loginInfo)
+                .onSuccess {
+                    if (it) {
+                        _existedUserEvent.value = Event(loginInfo)
+                    } else {
+                        _newUserEvent.value = Event(loginInfo)
+                    }
+                }.onFailure {
+                    // todo 로그전략 수립후 로그 찍어주기
+                }
+        }
+    }
+}
