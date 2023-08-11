6305d1ca9868dc3879f4ee1df383bf5a8d9ebaec
otter <koreanotter6@gmail.com>
2023-08-11 16:07:21 +0900

    [ui] 마이 페이지 / 마이 페이지 xml 작업 (#271)

--- android/app/src/main/java/com/teamtripdraw/android/ui/myPage/MyPageFragment.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/myPage/MyPageFragment.kt
@@ -1,19 +1,32 @@
 package com.teamtripdraw.android.ui.myPage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.teamtripdraw.android.R
+import com.teamtripdraw.android.BuildConfig
+import com.teamtripdraw.android.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
+
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding: FragmentMyPageBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMyPageBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
