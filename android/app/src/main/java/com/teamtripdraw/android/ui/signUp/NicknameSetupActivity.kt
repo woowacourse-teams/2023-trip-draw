4a8e5ce2e6c26dcc6c765670a9f39a91b06f15c6
changhwan <chang5hustle@gmail.com>
2023-08-11 18:18:19 +0900

    [feat] 소셜로그인 / 닉네임 설정 뷰모델,액티비티 로직 수정 (#266)

--- android/app/src/main/java/com/teamtripdraw/android/ui/signUp/NicknameSetupActivity.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/signUp/NicknameSetupActivity.kt
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import com.teamtripdraw.android.R
 import com.teamtripdraw.android.databinding.ActivityNicknameSetupBinding
 import com.teamtripdraw.android.support.framework.presentation.event.EventObserver
+import com.teamtripdraw.android.support.framework.presentation.getParcelableExtraCompat
 import com.teamtripdraw.android.ui.common.tripDrawViewModelFactory
 import com.teamtripdraw.android.ui.main.MainActivity
 import com.teamtripdraw.android.ui.model.UiLoginInfo
@@ -18,6 +19,8 @@ class NicknameSetupActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNicknameSetupBinding
     private val viewModel: NicknameSetupViewModel by viewModels { tripDrawViewModelFactory }
 
+    private lateinit var uiLoginInfo: UiLoginInfo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_nickname_setup)
@@ -26,6 +29,17 @@ class NicknameSetupActivity : AppCompatActivity() {
         binding.nicknameSetupViewModel = viewModel
 
         setupNavigation()
+        initExtraData()
+        setLoginInfo()
+    }
+
+    private fun initExtraData() {
+        uiLoginInfo = intent.getParcelableExtraCompat<UiLoginInfo>(UI_LOGIN_INFO)
+            ?: throw IllegalStateException(WRONG_EXTRA_DATA_ERROR.format(UI_LOGIN_INFO))
+    }
+
+    private fun setLoginInfo() {
+        viewModel.initLoginInfo(uiLoginInfo)
     }
 
     private fun setupNavigation() {
@@ -43,6 +57,8 @@ class NicknameSetupActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val WRONG_EXTRA_DATA_ERROR = "잘못된 EXTRA_DATA가 입력되었습니다 %s"
+
         private const val UI_LOGIN_INFO = "UI_LOGIN_INFO"
 
         fun getIntent(context: Context, uiLoginInfo: UiLoginInfo): Intent =
