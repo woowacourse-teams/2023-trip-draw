80822cb3f9c117b159bb18be95639b0178312331
greeng00se <thegreengoose7@gmail.com>
2023-08-13 20:22:30 +0900

    [test] 비동기 테스트 수정 (#232)

--- backend/src/test/java/dev/tripdraw/application/draw/PostCreateEventHandlerIntegrationTest.java
+++ backend/src/test/java/dev/tripdraw/application/draw/PostCreateEventHandlerIntegrationTest.java
@@ -5,12 +5,11 @@ import static dev.tripdraw.test.TestFixture.여행;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.timeout;
 
 import dev.tripdraw.domain.post.PostCreateEvent;
 import dev.tripdraw.domain.post.PostRepository;
 import dev.tripdraw.domain.trip.TripRepository;
-import dev.tripdraw.test.TestSyncConfig;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -18,12 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.support.TransactionTemplate;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@ContextConfiguration(classes = TestSyncConfig.class)
 @SpringBootTest
 public class PostCreateEventHandlerIntegrationTest {
 
@@ -56,7 +53,7 @@ public class PostCreateEventHandlerIntegrationTest {
 
         // then
         then(routeImageGenerator)
-                .should(times(1))
+                .should(timeout(5000).times(1))
                 .generate(any(), any(), any(), any());
     }
 }
