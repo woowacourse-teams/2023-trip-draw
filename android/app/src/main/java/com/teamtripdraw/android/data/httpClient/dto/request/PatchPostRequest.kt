1d85ab7d24169fb69b97ea0e79ba6e453ae96316
otter <koreanotter6@gmail.com>
2023-08-16 09:39:03 +0900

    [refactor] 감상 기록 / 빠진 Json annotation 추가 (#304)

--- android/app/src/main/java/com/teamtripdraw/android/data/httpClient/dto/request/PatchPostRequest.kt
+++ android/app/src/main/java/com/teamtripdraw/android/data/httpClient/dto/request/PatchPostRequest.kt
@@ -1,6 +1,12 @@
 package com.teamtripdraw.android.data.httpClient.dto.request
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class PatchPostRequest(
+    @Json(name = "title")
     val title: String,
+    @Json(name = "writing")
     val writing: String,
 )
