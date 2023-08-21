26000fc0975f071a7bec67dafa329a4ce10a0ced
ReO <yt930310@gmail.com>
2023-08-21 18:54:19 +0900

    [refactor] 개행 추가 (#289)

--- backend/src/test/java/dev/tripdraw/domain/trip/TripDurationTest.java
+++ backend/src/test/java/dev/tripdraw/domain/trip/TripDurationTest.java
@@ -1,17 +1,21 @@
 package dev.tripdraw.domain.trip;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.Duration;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class TripDurationTest {
 
-    private final TripDuration tripDuration = TripDuration.of(Duration.ofMinutes(3248));
+    private final LocalDateTime localDateTime = LocalDateTime.of(2023, 8, 3, 4, 0);
+    private final Point startingPoint = new Point(1.1, 1.1, localDateTime);
+    private final Point arrivalPoint = new Point(1.1, 1.1, localDateTime.plusMinutes(3248));
+    private final TripDuration tripDuration = TripDuration.of(startingPoint, arrivalPoint);
 
     @Test
     void 여행기간을_분_단위로_나타낸다() {
@@ -35,6 +39,7 @@ class TripDurationTest {
     void 여행기간을_일과_시간과_분_단위로_나타낸다() {
         // given & when
         String durationInDaysAndHoursAndMinutes = tripDuration.durationInDaysAndHoursAndMinutes();
+
         // then
         assertThat(durationInDaysAndHoursAndMinutes).isEqualTo("2일 6시간 8분");
     }
