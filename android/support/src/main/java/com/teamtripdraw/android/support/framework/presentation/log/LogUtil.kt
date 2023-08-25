d13facd637583fd67960a3fc98d6d2e0e2f78549
changhwan <chang5hustle@gmail.com>
2023-08-25 20:34:39 +0900

    [feat] general log 작성(#310)

--- android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/log/LogUtil.kt
+++ android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/log/LogUtil.kt
@@ -1,6 +1,6 @@
 package com.teamtripdraw.android.support.framework.presentation.log
 
 interface LogUtil {
+    fun general(throwable: Throwable?, message: String? = null)
     fun httpClient()
-    fun general(className: String, functionName: String, message: String? = null)
 }
