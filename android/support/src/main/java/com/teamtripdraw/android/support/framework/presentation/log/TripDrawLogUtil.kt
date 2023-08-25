86412ebe603108133f3e0f3a927b2726c1856600
changhwan <chang5hustle@gmail.com>
2023-08-25 20:34:39 +0900

    [feat] general log 작성(#310)

--- android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/log/TripDrawLogUtil.kt
+++ android/support/src/main/java/com/teamtripdraw/android/support/framework/presentation/log/TripDrawLogUtil.kt
@@ -1,34 +1,60 @@
 package com.teamtripdraw.android.support.framework.presentation.log
 
-import android.util.Log
 import com.teamtripdraw.android.support.BuildConfig.IS_RELEASE
+import io.sentry.Sentry
+import timber.log.Timber
 
 object TripDrawLogUtil : LogUtil {
 
-    private const val DEBUG_GENERAL_ERROR_LOG = "DEBUG_GENERAL_ERROR_LOG_%s"
-    private const val RELEASE_GENERAL_ERROR_LOG = "RELEASE_GENERAL_ERROR_LOG_%s"
-    private const val DEBUG_HTTP_CLIENT_ERROR_LOG = "DEBUG_HTTP_CLIENT_ERROR_LOG_%s"
-    private const val RELEASE_HTTP_CLIENT_ERROR_LOG = "DEBUG_HTTP_CLIENT_ERROR_LOG_%s"
-    private const val CLASS_NAME = "CLASS_NAME"
-    private const val MESSAGE = "MESSAGE"
+    private const val DEBUG_GENERAL_ERROR_LOG_FORMAT = "message : %s"
+    private const val DEBUG_HTTP_CLIENT_ERROR_LOG = "DEBUG_HTTP_CLIENT_ERROR_LOG"
+    private const val RELEASE_HTTP_CLIENT_ERROR_LOG = "DEBUG_HTTP_CLIENT_ERROR_LOG"
+    private const val SENTRY_MESSAGE_KEY = "ERROR_MESSAGE"
+    private const val NO_MESSAGE_NOTICE = "입력된 메시지가 없습니다."
 
-    override fun httpClient() {
-        // todo Sentry 연결후 관련 로그 작성
-    }
 
-    override fun general(className: String, functionName: String, message: String?) {
+//    override fun httpClient() {
+//        when (IS_RELEASE) {
+//            true -> httpClientReleaseErrorLog()
+//            false -> httpClientDebugErrorLog()
+//        }
+//    }
+//
+//    private fun httpClientReleaseErrorLog() {
+//        // todo Sentry 연결후 관련 로그 작성
+//    }
+//
+//    private fun httpClientDebugErrorLog() {
+//        Log.e(DEBUG_GENERAL_ERROR_LOG_FORMAT, functionName + (message ?: ""))
+//    }
+
+    override fun general(throwable: Throwable?, message: String?) {
         when (IS_RELEASE) {
-            true -> generalReleaseErrorLog(className, functionName, message)
-            false -> generalDebugErrorLog(className, functionName, message)
+            true -> generalReleaseErrorLog(throwable, message)
+            false -> generalDebugErrorLog(throwable, message)
         }
     }
 
-    private fun generalReleaseErrorLog(className: String, functionName: String, message: String?) {
-        // todo Sentry 연결후 관련 로그 작성
+    private fun generalReleaseErrorLog(throwable: Throwable?, message: String?) {
+        when (throwable == null) {
+            true -> {
+                Sentry.captureMessage(message ?: NO_MESSAGE_NOTICE)
+            }
+            false -> {
+                Sentry.captureException(throwable) { scope ->
+                    scope.setContexts(SENTRY_MESSAGE_KEY, message ?: NO_MESSAGE_NOTICE)
+                }
+            }
+        }
     }
 
-    private fun generalDebugErrorLog(className: String, functionName: String, message: String?) {
-        // todo Sentry 연결후 관련 로그 작성
-        Log.e(DEBUG_GENERAL_ERROR_LOG, functionName + (message ?: ""))
+    private fun generalDebugErrorLog(throwable: Throwable?, message: String?) {
+        when (throwable == null) {
+            true -> Timber.e(DEBUG_GENERAL_ERROR_LOG_FORMAT.format(message ?: NO_MESSAGE_NOTICE))
+            false -> Timber.e(
+                throwable,
+                DEBUG_GENERAL_ERROR_LOG_FORMAT.format(message ?: NO_MESSAGE_NOTICE),
+            )
+        }
     }
 }
