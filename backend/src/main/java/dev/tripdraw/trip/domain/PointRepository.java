79bda0698de35b35d25e25dad2d98b31fc5c8292
Combi153 <jcm04@naver.com>
2023-09-01 13:12:29 +0900

    [style] 코드 스타일 변경 (#276)

--- backend/src/main/java/dev/tripdraw/trip/domain/PointRepository.java
+++ backend/src/main/java/dev/tripdraw/trip/domain/PointRepository.java
@@ -1,9 +1,9 @@
 package dev.tripdraw.trip.domain;
 
+import static dev.tripdraw.trip.exception.TripExceptionType.POINT_NOT_FOUND;
+
 import dev.tripdraw.trip.exception.TripException;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import static dev.tripdraw.trip.exception.TripExceptionType.POINT_NOT_FOUND;
 
 public interface PointRepository extends JpaRepository<Point, Long> {
 
