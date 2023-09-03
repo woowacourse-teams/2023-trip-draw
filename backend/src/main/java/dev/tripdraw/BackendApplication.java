757335e242248268ec337af4abe817583847904e
greeng00se <thegreengoose7@gmail.com>
2023-09-03 16:49:37 +0900

    [feat] configuration processor 설정 추가 (#337)

--- backend/src/main/java/dev/tripdraw/BackendApplication.java
+++ backend/src/main/java/dev/tripdraw/BackendApplication.java
@@ -2,7 +2,9 @@ package dev.tripdraw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class BackendApplication {
 
