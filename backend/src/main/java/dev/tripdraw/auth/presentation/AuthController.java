c6b2f6ff67a5f91c91338dabea503d85014852f3
greeng00se <thegreengoose7@gmail.com>
2023-09-15 01:30:14 +0900

    [refactor] AuthService 분리 (#349)

--- backend/src/main/java/dev/tripdraw/auth/presentation/AuthController.java
+++ backend/src/main/java/dev/tripdraw/auth/presentation/AuthController.java
@@ -1,6 +1,6 @@
 package dev.tripdraw.auth.presentation;
 
-import dev.tripdraw.auth.application.AuthService;
+import dev.tripdraw.auth.application.AuthFacadeService;
 import dev.tripdraw.auth.dto.OauthRequest;
 import dev.tripdraw.auth.dto.OauthResponse;
 import dev.tripdraw.auth.dto.RegisterRequest;
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthFacadeService authFacadeService;
 
     @Operation(summary = "소셜 로그인 API", description = "소셜 로그인을 합니다.")
     @ApiResponse(
@@ -29,7 +29,7 @@ public class AuthController {
     )
     @PostMapping("/oauth/login")
     public ResponseEntity<OauthResponse> login(@RequestBody OauthRequest oauthRequest) {
-        OauthResponse oauthResponse = authService.login(oauthRequest);
+        OauthResponse oauthResponse = authFacadeService.login(oauthRequest);
         return ResponseEntity.ok(oauthResponse);
     }
 
@@ -40,7 +40,7 @@ public class AuthController {
     )
     @PostMapping("/oauth/register")
     public ResponseEntity<OauthResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
-        OauthResponse oauthResponse = authService.register(registerRequest);
+        OauthResponse oauthResponse = authFacadeService.register(registerRequest);
         return ResponseEntity.ok(oauthResponse);
     }
 
@@ -51,7 +51,7 @@ public class AuthController {
     )
     @PostMapping("/oauth/refresh")
     public ResponseEntity<OauthResponse> refresh(@Valid @RequestBody TokenRefreshRequest tokenRefreshRequest) {
-        OauthResponse oauthResponse = authService.refresh(tokenRefreshRequest);
+        OauthResponse oauthResponse = authFacadeService.refresh(tokenRefreshRequest);
         return ResponseEntity.ok(oauthResponse);
     }
 }
