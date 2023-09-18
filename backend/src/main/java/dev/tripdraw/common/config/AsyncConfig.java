3af4ac0cdda1805b1bff098355be32a47fca2735
greeng00se <thegreengoose7@gmail.com>
2023-09-18 14:26:54 +0900

    [feat] 비동기 예외 처리 (#368)

--- backend/src/main/java/dev/tripdraw/common/config/AsyncConfig.java
+++ backend/src/main/java/dev/tripdraw/common/config/AsyncConfig.java
@@ -1,9 +1,37 @@
 package dev.tripdraw.common.config;
 
+import dev.tripdraw.common.async.AsyncConfigurationProperties;
+import dev.tripdraw.common.async.AsyncExceptionHandler;
+import dev.tripdraw.common.async.MdcTaskDecorator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@RequiredArgsConstructor
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+
+    private final AsyncConfigurationProperties properties;
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(properties.coreSize());
+        executor.setMaxPoolSize(properties.maxSize());
+        executor.setQueueCapacity(properties.queueCapacity());
+        executor.setTaskDecorator(new MdcTaskDecorator());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
 }
