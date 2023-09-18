c1d125590bd235c40aaae0004fb8fe8402f2d2a1
pingu244 <pingu244@naver.com>
2023-09-18 15:37:35 +0900

    [ui] 전체 조회 / 여행 전체 조회 화면 작업 (#365)
    - item 만들기
    - recyclerView 연결

--- android/app/src/main/java/com/teamtripdraw/android/ui/allTrips/AllTripsFragment.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/allTrips/AllTripsFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.teamtripdraw.android.databinding.FragmentAllTripsBinding
+import com.teamtripdraw.android.ui.common.tripDrawViewModelFactory
 
 class AllTripsFragment : Fragment() {
 
     private var _binding: FragmentAllTripsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var adapter: AllTripsAdapter
+    private val viewModel: AllTripsViewModel by viewModels { tripDrawViewModelFactory }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +25,34 @@ class AllTripsFragment : Fragment() {
         _binding = FragmentAllTripsBinding.inflate(inflater)
         binding.lifecycleOwner = viewLifecycleOwner
 
+        bindViewModel()
+        initTripsObserve()
+        setAdapter()
+
         return binding.root
     }
+
+    private fun bindViewModel() {
+        binding.allTripsViewModel = viewModel
+    }
+
+    private fun initTripsObserve() {
+        viewModel.trips.observe(viewLifecycleOwner) {
+            adapter.submitList(it.tripItems)
+        }
+    }
+
+    private fun setAdapter() {
+        adapter = AllTripsAdapter(viewModel)
+        binding.rvAllTrips.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchTrips()
+    }
+
+    private fun fetchTrips() = viewModel.fetchTrips()
 
     override fun onDestroy() {
         super.onDestroy()
