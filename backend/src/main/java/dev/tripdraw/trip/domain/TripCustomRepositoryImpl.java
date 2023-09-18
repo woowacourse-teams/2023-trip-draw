5cdaf4738c7ce546039d0d2ec30ca8cdede949e2
Combi153 <jcm04@naver.com>
2023-09-18 17:46:44 +0900

    [refactor] TripCustomRepositoryImpl 로직 수정 (#361)

--- backend/src/main/java/dev/tripdraw/trip/domain/TripCustomRepositoryImpl.java
+++ backend/src/main/java/dev/tripdraw/trip/domain/TripCustomRepositoryImpl.java
@@ -7,10 +7,12 @@ import static dev.tripdraw.trip.domain.QTrip.trip;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import dev.tripdraw.common.domain.Paging;
+import io.micrometer.common.util.StringUtils;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
 @RequiredArgsConstructor
 @Repository
@@ -32,7 +34,7 @@ public class TripCustomRepositoryImpl implements TripCustomRepository {
                         addressLike(searchConditions.address())
                 )
                 .orderBy(trip.id.desc())
-                .limit(paging.limit() + 1)
+                .limit(paging.limit().longValue() + 1L)
                 .fetch();
     }
 
@@ -44,28 +46,28 @@ public class TripCustomRepositoryImpl implements TripCustomRepository {
     }
 
     private BooleanExpression yearIn(Set<Integer> years) {
-        if (years.isEmpty()) {
+        if (CollectionUtils.isEmpty(years)) {
             return null;
         }
         return point.recordedAt.year().in(years);
     }
 
     private BooleanExpression monthIn(Set<Integer> months) {
-        if (months.isEmpty()) {
+        if (CollectionUtils.isEmpty(months)) {
             return null;
         }
         return point.recordedAt.month().in(months);
     }
 
     private BooleanExpression dayOfWeekIn(Set<Integer> daysOfWeek) {
-        if (daysOfWeek.isEmpty()) {
+        if (CollectionUtils.isEmpty(daysOfWeek)) {
             return null;
         }
         return point.recordedAt.dayOfWeek().in(daysOfWeek);
     }
 
     private BooleanExpression addressLike(String address) {
-        if (address.isEmpty()) {
+        if (StringUtils.isBlank(address)) {
             return null;
         }
         return post.address.like(address + "%");
