dd991128b1709ce9952f88d4d69b4dd4bdc7e542
pingu244 <pingu244@naver.com>
2023-09-19 20:52:17 +0900

    [feat] 전체 조회 / 감상 조회 데이터 통신 모델 추가 (#376)

---
+++ android/app/src/main/java/com/teamtripdraw/android/data/httpClient/dto/request/AllPostsRequest.kt
@@ -0,0 +1,48 @@
+package com.teamtripdraw.android.data.httpClient.dto.request
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class AllPostsRequest(
+    @Json(name = "condition")
+    val condition: Condition,
+    @Json(name = "paging")
+    val paging: Paging,
+)
+
+@JsonClass(generateAdapter = true)
+data class Condition(
+    @Json(name = "address")
+    val address: String,
+    @Json(name = "ageRanges")
+    val ageRanges: List<Int>,
+    @Json(name = "currentPosition")
+    val currentPosition: CurrentPosition,
+    @Json(name = "daysOfWeek")
+    val daysOfWeek: List<Int>,
+    @Json(name = "genders")
+    val genders: List<Int>,
+    @Json(name = "hours")
+    val hours: List<Int>,
+    @Json(name = "months")
+    val months: List<Int>,
+    @Json(name = "years")
+    val years: List<Int>,
+)
+
+@JsonClass(generateAdapter = true)
+data class CurrentPosition(
+    @Json(name = "latitude")
+    val latitude: Double,
+    @Json(name = "longitude")
+    val longitude: Double,
+)
+
+@JsonClass(generateAdapter = true)
+data class Paging(
+    @Json(name = "lastViewedId")
+    val lastViewedId: Int,
+    @Json(name = "limit")
+    val limit: Int,
+)
