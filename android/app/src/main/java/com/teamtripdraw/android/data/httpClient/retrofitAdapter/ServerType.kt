a4638bc809dd2fe8673cc308a22494ac77d31dd2
otter <koreanotter6@gmail.com>
2023-09-22 17:42:45 +0900

    [refactor] 오류 처리를 위한 servertype에 naver reverse geocoder base url 적용 (#335)

--- android/app/src/main/java/com/teamtripdraw/android/data/httpClient/retrofitAdapter/ServerType.kt
+++ android/app/src/main/java/com/teamtripdraw/android/data/httpClient/retrofitAdapter/ServerType.kt
@@ -4,13 +4,13 @@ import com.teamtripdraw.android.BuildConfig
 
 enum class ServerType(private val baseURl: String) {
     TRIP_DRAW(BuildConfig.TRIP_DRAW_BASE_URL),
-    NAVER_GEO_CODER("수달이 채워넣을 부분"),
+    NAVER_REVERSE_GEOCODER(BuildConfig.NAVER_REVERS_GEOCODER_BASE_URL_STRING),
     ;
 
     companion object {
         private const val UNKOWN_BASE_URL_ERROR = """
         입력하신 BaseURl에 일치하는 서버 Type가 정의되어 있지않습니다.
-        정의되어있느 BaseURL을 확인하세요
+        정의되어있는 BaseURL을 확인하세요.
         """
 
         fun getByBaseUrl(baseURL: String): ServerType =
