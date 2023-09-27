fd638e48b67bff1da3b81779c2aad88d2595089f
Combi153 <jcm04@naver.com>
2023-09-27 13:40:50 +0900

    [chore] rebase 시 conflict 해결 (#403)

--- backend/src/main/java/dev/tripdraw/member/exception/MemberExceptionType.java
+++ backend/src/main/java/dev/tripdraw/member/exception/MemberExceptionType.java
@@ -1,6 +1,7 @@
 package dev.tripdraw.member.exception;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import dev.tripdraw.common.exception.ExceptionType;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum MemberExceptionType implements ExceptionType {
     DUPLICATE_NICKNAME(CONFLICT, "이미 존재하는 닉네임입니다."),
     MEMBER_NOT_FOUND(NOT_FOUND, "존재하지 않는 회원입니다."),
+    MEMBER_NOT_REGISTERED(INTERNAL_SERVER_ERROR, "가입이 완료되지 않은 회원이 존재합니다."),
     ;
 
     private final HttpStatus httpStatus;
