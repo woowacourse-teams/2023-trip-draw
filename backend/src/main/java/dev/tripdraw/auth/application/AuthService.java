6ab5a57ac0fbfbb63dc3dc475174e933edbc1bc9
greeng00se <thegreengoose7@gmail.com>
2023-10-04 17:19:13 +0900

    [refactor] AuthService 프로그래밍적 트랜잭션 제거 (#442)

--- backend/src/main/java/dev/tripdraw/auth/application/AuthService.java
+++ backend/src/main/java/dev/tripdraw/auth/application/AuthService.java
@@ -1,13 +1,17 @@
 package dev.tripdraw.auth.application;
 
+import static dev.tripdraw.auth.exception.AuthExceptionType.INVALID_TOKEN;
 import static dev.tripdraw.member.exception.MemberExceptionType.DUPLICATE_NICKNAME;
 import static dev.tripdraw.member.exception.MemberExceptionType.MEMBER_NOT_FOUND;
 
+import dev.tripdraw.auth.domain.RefreshToken;
+import dev.tripdraw.auth.domain.RefreshTokenRepository;
 import dev.tripdraw.auth.dto.OauthInfo;
+import dev.tripdraw.auth.dto.OauthResponse;
+import dev.tripdraw.auth.exception.AuthException;
 import dev.tripdraw.member.domain.Member;
 import dev.tripdraw.member.domain.MemberRepository;
 import dev.tripdraw.member.exception.MemberException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,17 +21,30 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AuthService {
 
-    private final MemberRepository memberRepository;
+    private static final OauthResponse EMPTY_TOKEN = new OauthResponse("", "");
 
-    public Optional<Member> login(OauthInfo oauthInfo) {
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public OauthResponse login(OauthInfo oauthInfo) {
         return memberRepository.findByOauthIdAndOauthType(oauthInfo.oauthId(), oauthInfo.oauthType())
-                .or(() -> {
+                .map(member -> generateToken(member.id()))
+                .orElseGet(() -> {
                     memberRepository.save(Member.of(oauthInfo.oauthId(), oauthInfo.oauthType()));
-                    return Optional.empty();
+                    return EMPTY_TOKEN;
                 });
     }
 
-    public Optional<Member> register(OauthInfo oauthInfo, String nickname) {
+    private OauthResponse generateToken(Long memberId) {
+        String accessToken = jwtTokenProvider.generateAccessToken(memberId.toString());
+        String refreshToken = jwtTokenProvider.generateRefreshToken();
+        refreshTokenRepository.deleteByMemberId(memberId);
+        refreshTokenRepository.save(new RefreshToken(memberId, refreshToken));
+        return new OauthResponse(accessToken, refreshToken);
+    }
+
+    public OauthResponse register(OauthInfo oauthInfo, String nickname) {
         if (memberRepository.existsByNickname(nickname)) {
             throw new MemberException(DUPLICATE_NICKNAME);
         }
@@ -35,6 +52,13 @@ public class AuthService {
         Member member = memberRepository.findByOauthIdAndOauthType(oauthInfo.oauthId(), oauthInfo.oauthType())
                 .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
         member.changeNickname(nickname);
-        return Optional.of(member);
+        return generateToken(member.id());
+    }
+
+    public OauthResponse refresh(String token) {
+        jwtTokenProvider.validateRefreshToken(token);
+        RefreshToken refreshToken = refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> new AuthException(INVALID_TOKEN));
+        return generateToken(refreshToken.memberId());
     }
 }
