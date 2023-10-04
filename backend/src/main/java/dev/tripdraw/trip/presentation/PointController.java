6aeda63dea52aabddb712719f8a39cc9dabbb5a1
greeng00se <thegreengoose7@gmail.com>
2023-10-04 16:02:17 +0900

    [refactor] PointService 분리 (#440)

--- backend/src/main/java/dev/tripdraw/trip/presentation/PointController.java
+++ backend/src/main/java/dev/tripdraw/trip/presentation/PointController.java
@@ -5,7 +5,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 import dev.tripdraw.common.auth.Auth;
 import dev.tripdraw.common.auth.LoginUser;
 import dev.tripdraw.common.swagger.SwaggerAuthorizationRequired;
-import dev.tripdraw.trip.application.TripService;
+import dev.tripdraw.trip.application.PointService;
 import dev.tripdraw.trip.dto.PointCreateRequest;
 import dev.tripdraw.trip.dto.PointCreateResponse;
 import dev.tripdraw.trip.dto.PointResponse;
@@ -19,28 +19,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Point", description = "위치정보 관련 API 명세")
 @SwaggerAuthorizationRequired
 @RequiredArgsConstructor
+@RequestMapping("/points")
 @RestController
 public class PointController {
 
-    private final TripService tripService;
+    private final PointService pointService;
 
     @Operation(summary = "위치 정보 저장 API", description = "현재 진행 중인 여행의 경로에 위치 정보를 저장합니다.")
     @ApiResponse(
             responseCode = "201",
             description = "위치 정보 저장 성공."
     )
-    @PostMapping("/points")
+    @PostMapping
     public ResponseEntity<PointCreateResponse> addPoint(
             @Auth LoginUser loginUser,
             @RequestBody PointCreateRequest pointCreateRequest
     ) {
-        PointCreateResponse response = tripService.addPoint(loginUser, pointCreateRequest);
+        PointCreateResponse response = pointService.create(loginUser, pointCreateRequest);
         return ResponseEntity.status(CREATED).body(response);
     }
 
@@ -49,13 +51,13 @@ public class PointController {
             responseCode = "200",
             description = "위치 정보 조회 성공."
     )
-    @GetMapping("/points/{pointId}")
+    @GetMapping("/{pointId}")
     public ResponseEntity<PointResponse> readPointById(
             @Auth LoginUser loginUser,
             @PathVariable Long pointId,
             @RequestParam Long tripId
     ) {
-        PointResponse response = tripService.readPointByTripAndPointId(loginUser, tripId, pointId);
+        PointResponse response = pointService.read(loginUser, tripId, pointId);
         return ResponseEntity.ok(response);
     }
 
@@ -64,13 +66,13 @@ public class PointController {
             responseCode = "204",
             description = "위치정보 삭제 성공"
     )
-    @DeleteMapping("/points/{pointId}")
+    @DeleteMapping("/{pointId}")
     public ResponseEntity<Void> deletePoint(
             @Auth LoginUser loginUser,
             @PathVariable Long pointId,
             @RequestParam Long tripId
     ) {
-        tripService.deletePoint(loginUser, pointId, tripId);
+        pointService.delete(loginUser, pointId, tripId);
         return ResponseEntity.noContent().build();
     }
 }
