ad3784e821f59f258c8306abed8cb9cc17d7ad55
Combi153 <jcm04@naver.com>
2023-10-08 20:46:08 +0900

    [refactor] Multipart size 설정 시 정적 팩터리 메서드 사용 (#453)

--- backend/src/main/java/dev/tripdraw/common/config/MultipartConfig.java
+++ backend/src/main/java/dev/tripdraw/common/config/MultipartConfig.java
@@ -1,7 +1,5 @@
 package dev.tripdraw.common.config;
 
-import static org.springframework.util.unit.DataUnit.MEGABYTES;
-
 import jakarta.servlet.MultipartConfigElement;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
@@ -21,8 +19,8 @@ public class MultipartConfig {
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory multipartConfigFactory = new MultipartConfigFactory();
-        multipartConfigFactory.setMaxRequestSize(DataSize.of(20, MEGABYTES));
-        multipartConfigFactory.setMaxFileSize(DataSize.of(20, MEGABYTES));
+        multipartConfigFactory.setMaxRequestSize(DataSize.ofMegabytes(20));
+        multipartConfigFactory.setMaxFileSize(DataSize.ofMegabytes(20));
 
         return multipartConfigFactory.createMultipartConfig();
     }
