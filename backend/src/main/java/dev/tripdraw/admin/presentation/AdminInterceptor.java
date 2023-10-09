21e7473e7ff01456f2689f616ffe2a39b3d94119
greeng00se <thegreengoose7@gmail.com>
2023-10-09 23:01:13 +0900

    [feat] AdminInterceptor 추가 (#454)

---
+++ backend/src/main/java/dev/tripdraw/admin/presentation/AdminInterceptor.java
@@ -0,0 +1,33 @@
+package dev.tripdraw.admin.presentation;
+
+import static dev.tripdraw.admin.exception.AdminExceptionType.ADMIN_AUTH_FAIL;
+
+import dev.tripdraw.admin.application.AdminAuthService;
+import dev.tripdraw.admin.exception.AdminException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+@Component
+public class AdminInterceptor implements HandlerInterceptor {
+
+    private static final String SESSION = "SESSION";
+
+    private final AdminAuthService adminAuthService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new AdminException(ADMIN_AUTH_FAIL);
+        }
+
+        String uuid = (String) session.getAttribute(SESSION);
+        adminAuthService.validateSession(uuid);
+        return true;
+    }
+}
