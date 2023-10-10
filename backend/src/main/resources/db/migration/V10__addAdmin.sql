ebab7522432090aaee6ecfd7348c40e7511d5d66
greeng00se <thegreengoose7@gmail.com>
2023-10-10 16:19:09 +0900

    [chore] Flyway 설정 수정 (#454)

--- backend/src/main/resources/db/migration/V10__addAdmin.sql
+++ backend/src/main/resources/db/migration/V10__addAdmin.sql
@@ -1,18 +1,18 @@
-CREATE TABLE `Admin`
+CREATE TABLE `admin`
 (
     `created_at` TIMESTAMP,
     `admin_id`   BIGINT PRIMARY KEY AUTO_INCREMENT,
     `updated_at` TIMESTAMP,
     `email`      VARCHAR(255),
     `password`   VARCHAR(255),
-    `failCount`  BIGINT,
+    `fail_count` BIGINT
 );
 
-CREATE TABLE `Admin_Session`
+CREATE TABLE `admin_session`
 (
     `created_at`       TIMESTAMP,
     `admin_session_id` BIGINT PRIMARY KEY AUTO_INCREMENT,
     `updated_at`       TIMESTAMP,
     `uuid`             VARCHAR(255),
-    `expiredDateTime`  TIMESTAMP,
+    `expired_date_time` TIMESTAMP
 );
