3fb5cca626fdd0760b57aa0bba7fd0fb5bd9e5a9
greeng00se <thegreengoose7@gmail.com>
2023-10-12 02:04:19 +0900

    [test] Annotation 순서 변경 (#429)

--- backend/src/test/java/dev/tripdraw/test/ControllerTest.java
+++ backend/src/test/java/dev/tripdraw/test/ControllerTest.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestExecutionListeners;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestExecutionListeners(
         value = DatabaseCleaner.class,
         mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
 )
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class ControllerTest {
 
     @LocalServerPort
