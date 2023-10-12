870a59bb13e4616c4a528c66e287a1f138f43774
Combi153 <jcm04@naver.com>
2023-10-12 21:23:13 +0900

    [refactor] 사용하지 않는 DTO 제거 (#473)

--- backend/src/main/java/dev/tripdraw/trip/dto/TripSearchPaging.java
+++
@@ -1,9 +0,0 @@
-package dev.tripdraw.trip.dto;
-
-import dev.tripdraw.trip.query.TripPaging;
-
-public record TripSearchPaging(Long lastViewedId, Integer limit) {
-    public TripPaging toTripPaging() {
-        return new TripPaging(lastViewedId, limit);
-    }
-}
