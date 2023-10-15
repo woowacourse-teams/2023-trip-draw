944cab5b45e838adf9e07062c947400ea1b7dcea
pingu244 <pingu244@naver.com>
2023-10-16 01:27:29 +0900

    [refactor] 지도 바텀네비게이션 / 작성자따라 기록 작성, 위치 삭제 버튼 안보이는 기능 추가 (#490)

--- android/app/src/main/java/com/teamtripdraw/android/ui/common/bindingAdapter/IconBindingAdapter.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/common/bindingAdapter/IconBindingAdapter.kt
@@ -1,11 +1,10 @@
 package com.teamtripdraw.android.ui.common.bindingAdapter
 
 import android.view.View
-import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:setIconVisibility")
-fun ImageView.setIconVisibility(isMine: Boolean?) {
+fun View.setIconVisibility(isMine: Boolean?) {
     if (isMine == null) return
     if (isMine) {
         this.visibility = View.VISIBLE
