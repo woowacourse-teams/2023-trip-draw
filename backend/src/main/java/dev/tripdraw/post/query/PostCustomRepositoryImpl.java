be603e9a490a53a4c4b2f21e0caa96dc44f5d171
Combi153 <jcm04@naver.com>
2023-10-16 18:50:32 +0900

    [refactor] Post 동적 쿼리 Member fetch join 추가 (#489)

--- backend/src/main/java/dev/tripdraw/post/query/PostCustomRepositoryImpl.java
+++ backend/src/main/java/dev/tripdraw/post/query/PostCustomRepositoryImpl.java
@@ -1,14 +1,18 @@
 package dev.tripdraw.post.query;
 
+import static dev.tripdraw.common.util.QueryDslUtils.addressLike;
+import static dev.tripdraw.common.util.QueryDslUtils.dayOfWeekIn;
+import static dev.tripdraw.common.util.QueryDslUtils.hourIn;
+import static dev.tripdraw.common.util.QueryDslUtils.monthIn;
+import static dev.tripdraw.common.util.QueryDslUtils.postIdLt;
+import static dev.tripdraw.common.util.QueryDslUtils.yearIn;
 import static dev.tripdraw.post.domain.QPost.post;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import dev.tripdraw.post.domain.Post;
 import dev.tripdraw.post.dto.PostPaging;
 import dev.tripdraw.post.dto.PostSearchConditions;
 import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -16,72 +20,26 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class PostCustomRepositoryImpl implements PostCustomRepository {
 
+    private static final long UNIT_FOR_HAS_NEXT_PAGE = 1L;
+
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public List<Post> findAllByConditions(PostSearchConditions conditions, PostPaging paging) {
-        // TODO: 2023/09/16 연령대, 성별 추가
         return jpaQueryFactory.selectFrom(post)
-                .leftJoin(post.point).fetchJoin()
+                .innerJoin(post.point).fetchJoin()
+                .innerJoin(post.member).fetchJoin()
                 .where(
-                        postIdLt(paging.lastViewedId()),
                         yearIn(conditions.years()),
                         monthIn(conditions.months()),
                         dayOfWeekIn(conditions.daysOfWeek()),
                         hourIn(conditions.hours()),
-                        addressLike(conditions.address())
+                        addressLike(conditions.address()),
+                        postIdLt(paging.lastViewedId())
                 )
-                .limit(paging.limit().longValue() + 1L)
+                .limit(paging.limit().longValue() + UNIT_FOR_HAS_NEXT_PAGE)
                 .orderBy(post.id.desc())
                 .fetch();
-    }
-
-    private BooleanExpression postIdLt(Long lastViewedId) {
-        if (lastViewedId == null) {
-            return null;
-        }
-
-        return post.id.lt(lastViewedId);
-    }
-
-    private BooleanExpression yearIn(Set<Integer> years) {
-        if (years == null || years.isEmpty()) {
-            return null;
-        }
-
-        return post.point.recordedAt.year().in(years);
-    }
-
-    private BooleanExpression monthIn(Set<Integer> months) {
-        if (months == null || months.isEmpty()) {
-            return null;
-        }
-
-        return post.point.recordedAt.month().in(months);
-    }
-
-    private BooleanExpression dayOfWeekIn(Set<Integer> daysOfWeek) {
-        if (daysOfWeek == null || daysOfWeek.isEmpty()) {
-            return null;
-        }
-
-        return post.point.recordedAt.dayOfWeek().in(daysOfWeek);
-    }
-
-    private BooleanExpression hourIn(Set<Integer> hours) {
-        if (hours == null || hours.isEmpty()) {
-            return null;
-        }
-
-        return post.point.recordedAt.hour().in(hours);
-    }
-
-    private BooleanExpression addressLike(String address) {
-        if (address == null || address.isEmpty()) {
-            return null;
-        }
-
-        return post.address.like(address + "%");
     }
 
     @Override
@@ -89,7 +47,7 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
         return jpaQueryFactory.selectFrom(post)
                 .leftJoin(post.point).fetchJoin()
                 .where(postIdLt(paging.lastViewedId()))
-                .limit(paging.limit().longValue() + 1L)
+                .limit(paging.limit().longValue() + UNIT_FOR_HAS_NEXT_PAGE)
                 .orderBy(post.id.desc())
                 .fetch();
     }
