871600cf86a0c291b80b82c24be2a5352769b101
otter <koreanotter6@gmail.com>
2023-10-17 00:29:00 +0900

    [feat] 모두의 여행 / 로딩 아이템 추가 (#495)

--- android/app/src/main/java/com/teamtripdraw/android/ui/allTrips/AllTripsAdapter.kt
+++ android/app/src/main/java/com/teamtripdraw/android/ui/allTrips/AllTripsAdapter.kt
@@ -1,27 +1,34 @@
 package com.teamtripdraw.android.ui.allTrips
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.teamtripdraw.android.databinding.ItemLoadingBinding
 import com.teamtripdraw.android.ui.model.allTrips.UiAllTripItem
 import com.teamtripdraw.android.ui.model.allTrips.UiTripOfAll
 
 class AllTripsAdapter(
     private val viewModel: AllTripsViewModel,
-) : ListAdapter<UiAllTripItem, AllTripsViewHolder>(diffUtil) {
+) : ListAdapter<UiAllTripItem, RecyclerView.ViewHolder>(diffUtil) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllTripsViewHolder {
-        return AllTripsViewHolder.of(parent, viewModel)
-    }
-
-    override fun onBindViewHolder(holder: AllTripsViewHolder, position: Int) {
-        when (val item = getItem(position)) {
-            is UiTripOfAll -> holder.bind(item)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ALL_TRIP_ITEM_VIEW -> AllTripsViewHolder.of(parent, viewModel)
+            else -> LoadingViewHolder.of(parent)
         }
     }
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is AllTripsViewHolder) holder.bind(getItem(position) as UiTripOfAll)
+    }
+
     override fun getItemViewType(position: Int): Int {
-        return ALL_TRIP_ITEM_VIEW
+        return when (getItem(position)) {
+            is UiTripOfAll -> ALL_TRIP_ITEM_VIEW
+            else -> LOADING_VIEW
+        }
     }
 
     companion object {
@@ -44,6 +51,20 @@ class AllTripsAdapter(
                 if (oldItem !is UiTripOfAll || newItem !is UiTripOfAll) return false
                 return oldItem == newItem
             }
+        }
+    }
+
+    class LoadingViewHolder(val binding: ItemLoadingBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun of(parent: ViewGroup): LoadingViewHolder =
+                LoadingViewHolder(
+                    ItemLoadingBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false,
+                    ),
+                )
         }
     }
 }
