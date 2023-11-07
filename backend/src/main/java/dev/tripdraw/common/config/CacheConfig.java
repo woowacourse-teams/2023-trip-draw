a74f567cbb2f2ee9939b87ae9ef4888d13c253fa
Combi153 <jcm04@naver.com>
2023-11-07 10:24:35 +0900

    [refactor] 애노테이션 위치 변경 및 메서드 분리 (#537)

--- backend/src/main/java/dev/tripdraw/common/config/CacheConfig.java
+++ backend/src/main/java/dev/tripdraw/common/config/CacheConfig.java
@@ -1,15 +1,18 @@
 package dev.tripdraw.common.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import dev.tripdraw.common.cache.CacheType;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableCaching
 @Configuration
 public class CacheConfig {
 
@@ -22,10 +25,13 @@ public class CacheConfig {
 
     private List<CaffeineCache> caches() {
         return Arrays.stream(CacheType.values())
-                .map(cache -> new CaffeineCache(
-                        cache.cacheName(),
-                        Caffeine.newBuilder().maximumSize(cache.maximumSize()).build())
-                )
+                .map(cacheType -> new CaffeineCache(cacheType.cacheName(), cache(cacheType)))
                 .toList();
+    }
+
+    private Cache<Object, Object> cache(CacheType cacheType) {
+        return Caffeine.newBuilder()
+                .maximumSize(cacheType.maximumSize())
+                .build();
     }
 }
